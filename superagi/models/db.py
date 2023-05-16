472f7075e4a1ebee7f76e331b1863cd636b222b5
abhijeet <abhijeet@contlo.com>
2023-05-16 20:53:36 +0530

    Model Updated

--- superagi/models/db.py
+++ superagi/models/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 
 # Replace 'username' and 'password' with your PostgreSQL credentials
-db_username = 'abhijeet'
-db_password = 'password'
-db_name = 'test123'
+db_username = ''
+db_password = ''
+db_name = ''
 
 engine = None
 
