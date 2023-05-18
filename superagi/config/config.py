8cacda5de5ae562a5b157bbe403baf8542eb5054
rakesh-krishna-a-s <fluder.paradyne@proton.me>
2023-05-18 18:43:58 +0530

    added config getter

---
+++ superagi/config/config.py
@@ -0,0 +1,44 @@
+import os
+from pydantic import BaseSettings
+import yaml
+
+
+class Config(BaseSettings):
+    class Config:
+        env_file_encoding = "utf-8"
+        extra = "allow"  # Allow extra fields
+
+    @classmethod
+    def load_config(cls, config_file: str) -> dict:
+        # Fetch environment variables
+        env_vars = dict(os.environ)
+
+        # Read config file
+        with open(config_file, 'r') as file:
+            config_data = yaml.safe_load(file)
+
+        # Merge environment variables and config data
+        config_data = {**config_data, **env_vars}
+
+        return config_data
+
+    def __init__(self, config_file: str, **kwargs):
+        config_data = self.load_config(config_file)
+        super().__init__(**config_data, **kwargs)
+
+    def get_config(self, key: str, default: str = None) -> str:
+        return self.dict().get(key, default)
+
+
+_config_instance = Config("../config.yaml")
+
+
+def get_config(key: str, default: str = None) -> str:
+    """
+    Function to get the configuration value from the instance.
+
+    :param key: str, the key to retrieve the configuration value.
+    :param default: str, the default value to return if the key is not present.
+    :return: str, the configuration value.
+    """
+    return _config_instance.get_config(key, default)
