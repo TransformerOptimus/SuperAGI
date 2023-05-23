158bfd3f7e4187a0e4a1449e4bb26d9745a6d06b
COLONAYUSH <ayushkaps9462@gmail.com>
2023-05-23 17:25:57 +0530

    Added auto creation of virtual env in dir

--- run.bat
+++ run.bat
@@ -1,15 +1,32 @@
 @echo off
-
-REM Check if config.yaml file exists
-if not exist "config.yaml" (
-    echo ERROR: config.yaml file not found. Please create the config.yaml file using the command 'copy config_template.yaml config.yaml' and edit it.
+echo Checking if config.yaml file exists...
+if not exist config.yaml (
+    echo ERROR: config.yaml file not found. Please create the config.yaml file.
     exit /b 1
 )
-
-REM Install requirements
-echo Installing requirements...
-pip install -r requirements.txt
-
-REM Run test.py
-echo Running test.py...
+echo Checking if virtual environment is activated...
+if not defined VIRTUAL_ENV (
+    echo Virtual environment not activated. Creating and activating virtual environment...
+    python3 -m venv venv
+    if errorlevel 1 (
+      echo Error: Failed to create virtual environment.
+      exit /b 1
+    )
+    call venv\Scripts\activate.bat
+) else (
+    echo Virtual environment is already activated.
+)
+echo Checking requirements...
+pip show -r requirements.txt >nul 2>&1
+if errorlevel 1 (
+    echo Installing requirements...
+    pip install -r requirements.txt >nul 2>&1
+) else (
+    echo All packages are already installed.
+)
+echo Running test.py with python...
 python test.py
+if errorlevel 1 (
+    echo Running test.py with python3...
+    python3 test.py
+)
