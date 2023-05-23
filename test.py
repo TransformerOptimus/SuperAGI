4bf2dd5afcc96385687d2a2cb3ff1cf2ffa04a15
rakesh-krishna-a-s <fluder.paradyne@proton.me>
2023-05-23 17:42:40 +0530

    add multiple goals

--- test.py
+++ test.py
@@ -37,5 +37,5 @@ tools = [
 
 superagi = SuperAgi.from_llm_and_tools("Super AGI", "To solve any complex problems for you", memory, tools, OpenAi(model="gpt-4"))
 user_goal=[]
-user_goal=str(input("Enter your Goal:\n"))
-superagi.execute([user_goal])
+user_goal=str(input("Enter your Goals seperated by ',':\n")).split(",")
+superagi.execute(user_goal)
