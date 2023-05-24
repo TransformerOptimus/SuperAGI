f35ee4c5e33c863d7d3b76ed01aac70929eb78e2
rakesh-krishna-a-s <fluder.paradyne@proton.me>
2023-05-24 18:36:20 +0530

    fix: exit bugs

--- run_gui.sh
+++ run_gui.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
-cd gui
 
-command -v node >/dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo "Node.js is installed. Proceeding with npm commands..."
+api_process=""
+ui_process=""
 
-  # Run npm install, build, and start
+function check_command() {
+  command -v "$1" >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "$1 is not installed. Please install $1 to proceed."
+    exit 1
+  fi
+}
+
+function run_npm_commands() {
   cd gui
   npm install
   if [ $? -ne 0 ]; then
     echo "Error during 'npm install'. Exiting."
-    # exit 1
+    exit 1
   fi
 
   npm run build
   if [ $? -ne 0 ]; then
-    echo "Error during 'npm build'. Exiting."
-    # exit 1
+    echo "Error during 'npm run build'. Exiting."
+    exit 1
   fi
-else
-  echo "Node.js is not installed. Please install Node.js to proceed."
-fi
 
-cd ..
+  cd ..
+}
 
-cd . && uvicorn main:app --host 0.0.0.0 --port 8000 &
-cd gui && npm run dev
+function run_server() {
+  uvicorn main:app --host 0.0.0.0 --port 8000 &
+  api_process=$!
+  cd gui && npm run dev &
+  ui_process=$!
+}
+
+function cleanup() {
+  echo "Shutting down processes..."
+  kill $api_process
+  kill $ui_process
+  echo "Processes terminated. Exiting."
+  exit 1
+}
+
+trap cleanup SIGINT
+
+check_command "node"
+check_command "npm"
+check_command "uvicorn"
+
+run_npm_commands
+run_server
+
+wait $api_process
