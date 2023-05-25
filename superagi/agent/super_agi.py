96c094aaa3157f92b907448dc3c9a916987c2775
luciferlinx <luciferlinx101@gmail.com>
2023-05-25 18:35:56 +0530

    Apis updated & celery added

--- superagi/agent/super_agi.py
+++ superagi/agent/super_agi.py
@@ -17,14 +17,41 @@ from superagi.vector_store.base import VectorStore
 from superagi.vector_store.document import Document
 import json
 from halo import Halo
+from superagi.models.types.agent_with_config import AgentWithConfig
+from superagi.models.agent_execution_feed import AgentExecutionFeed
+from superagi.models.agent_execution import AgentExecution
+from sqlalchemy.exc import SQLAlchemyError
 
 
+from typing import Any
+
+from superagi.models.db import connectDB
+from sqlalchemy.orm import sessionmaker, query
 
 
 FINISH = "finish"
 # print("\033[91m\033[1m"
 #         + "\nA bit about me...."
 #         + "\033[0m\033[0m")
+
+
+engine = connectDB()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def checkExecution(execution_id):
+    try:
+        execution = session.query(AgentExecution).filter_by(id=execution_id).first()
+        if execution and execution.status in ['PAUSED', 'COMPLETED']:
+            return False
+        else:
+            return True
+    except SQLAlchemyError as e:
+        print("Error occurred during execution status check:", e)
+        return False
+    finally:
+        session.close()
+
 
 class SuperAgi:
     def __init__(self,
@@ -33,6 +60,7 @@ class SuperAgi:
                  llm: BaseLlm,
                  memory: VectorStore,
                  tools: List[BaseTool],
+                 agent_config:Any,
                  output_parser: BaseOutputParser = AgentOutputParser(),
                  ):
         self.ai_name = ai_name
@@ -42,6 +70,9 @@ class SuperAgi:
         self.memory = memory
         self.output_parser = output_parser
         self.tools = tools
+        self.agent_config = agent_config
+        
+        #Init Log
         print("\033[92m\033[1m" + "\nWelcome to SuperAGI - The future of AGI" + "\033[0m\033[0m")
 
 
@@ -70,7 +101,11 @@ class SuperAgi:
         )
         iteration = 10
         i = 0
-        while True:
+        while True and checkExecution(execution_id=self.agent_config["agent_execution_id"]):
+            # if checkExecution(execution_id=self.agent_config["agent_execution_id"]) == False:
+            #     break
+            
+
             format_prefix_yellow = "\033[93m\033[1m"
             format_suffix_yellow = "\033[0m\033[0m"
             format_prefix_green = "\033[92m\033[1m"
@@ -80,18 +115,27 @@ class SuperAgi:
             if i > iteration:
                 return
             # print(self.tools)
-            autogpt_prompt = AgentPromptBuilder.get_autogpt_prompt(self.ai_name, self.ai_role, goals, self.tools)
+            autogpt_prompt = AgentPromptBuilder.get_autogpt_prompt(self.ai_name, self.ai_role, goals, self.tools,self.agent_config)
             autogpt_prompt_to_print = AgentPromptToPrintBuilder.get_autogpt_prompt(self.ai_name, self.ai_role, goals, self.tools)
+
             # generated_prompt = self.get_analytics_insight_prompt(analytics_string)
+            
             messages = [{"role": "system", "content": autogpt_prompt},
                        {"role": "system", "content": f"The current time and date is {time.strftime('%c')}"}]
+            
+            #Saving to database
+            for messaege in messages:
+                agent_execution_feed = AgentExecutionFeed(agent_execution_id=self.agent_config["agent_execution_id"],agent_id=self.agent_config["agent_id"],feed=messaege["content"],role=messaege["role"])
+                session.add(agent_execution_feed)
+                session.commit()
 
             for history in self.full_message_history[-10:]:
                 # print(history.type + " : ", history.content)
                 messages.append({"role": history.type, "content": history.content})
 
-            # print(autogpt_prompt)
-            print(autogpt_prompt_to_print)
+            print(autogpt_prompt)
+            # print(autogpt_prompt_to_print)
+
             # Discontinue if continuous limit is reached
             current_tokens = TokenCounter.count_message_tokens(messages, self.llm.get_model())
             token_limit = TokenCounter.token_limit(self.llm.get_model())
@@ -107,7 +151,15 @@ class SuperAgi:
 
             # Print Assistant thoughts
             self.full_message_history.append(HumanMessage(content=user_input))
+            agent_execution_feed = AgentExecutionFeed(agent_execution_id=self.agent_config["agent_execution_id"],agent_id=self.agent_config["agent_id"],feed=user_input,role="user")
+            session.add(agent_execution_feed)
+            session.commit()
+
             self.full_message_history.append(AIMessage(content=assistant_reply))
+            agent_execution_feed = AgentExecutionFeed(agent_execution_id=self.agent_config["agent_execution_id"],agent_id=self.agent_config["agent_id"],feed=assistant_reply,role="assistant")
+            session.add(agent_execution_feed)
+            session.commit()
+
 
             # print(assistant_reply)
             action = self.output_parser.parse(assistant_reply)
@@ -141,10 +193,15 @@ class SuperAgi:
             print(format_prefix_yellow + "Tool Response : " + format_suffix_yellow + result + "\n")
             #self.memory.add_documents([Document(text_content=assistant_reply)])
             self.full_message_history.append(SystemMessage(content=result))
+            
+            agent_execution_feed = AgentExecutionFeed(agent_execution_id=self.agent_config["agent_execution_id"],agent_id=self.agent_config["agent_id"],feed=result,role="system")
+            session.add(agent_execution_feed)
+            session.commit()
+
+
             # print(self.full_message_history)
             
             print(format_prefix_green + "Iteration completed moving to next iteration!" + format_suffix_green)
-        pass
 
     def call_llm(self):
         pass
