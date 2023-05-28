42bc96723e4da52381108c03f7539cc8a7ccbc74
TransformerOptimus <muknrq@gmail.com>
2023-05-28 11:30:22 +0530

    fixin ui and cli2

--- ui.py
+++ ui.py
@@ -24,10 +24,10 @@ def run_npm_commands(shell=False):
 
 def run_server(shell=False):
     api_process = subprocess.Popen(["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"], shell=shell)
-    celery_process = None
-    # celery_process = subprocess.Popen(["celery", "-A", "superagi.worker", "worker", "--loglevel=info"], shell=shell)
+    # celery_process = None
+    celery_process = subprocess.Popen(["celery", "-A", "superagi.worker", "worker", "--loglevel=info"], shell=shell)
     os.chdir("gui")
-    ui_process = None #subprocess.Popen(["npm", "run", "dev"], shell=shell)
+    ui_process = subprocess.Popen(["npm", "run", "dev"], shell=shell)
     os.chdir("..")
     return api_process, ui_process , celery_process
 
