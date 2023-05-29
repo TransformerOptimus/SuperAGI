6dd9af7e1776c890e4266a823add0cb7a9ba49ce
Arkajit-Datta <arkajitdatta2010@gmail.com>
2023-05-30 01:53:03 +0530

    Added testing-code for Slack Tool

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ from superagi.tools.twitter.send_tweet import SendTweetTool
 from superagi.tools.email.read_email import ReadEmailTool
 from superagi.tools.email.send_email import SendEmailTool
 from superagi.tools.email.send_email_attachment import SendEmailAttachmentTool
+from superagi.tools.slack.read_messages import SlackReadMessageTool
+from superagi.tools.slack.send_message import SlackMessageTool
+from superagi.tools.slack.tool import SlackTool
 from superagi.vector_store.embedding.openai import OpenAiEmbedding
 from superagi.vector_store.vector_factory import VectorFactory
 
@@ -31,7 +34,9 @@ tools = [
     ReadFileTool(),
     ReadEmailTool(),
     SendEmailTool(),
-    SendEmailAttachmentTool()
+    SendEmailAttachmentTool(),
+    SlackMessageTool(),
+    SlackReadMessageTool()
     # GoogleSerpTool()
 ]
 
@@ -41,6 +46,17 @@ tools = [
 # send_tool = SendTweetTool()
 # send_tool.execute("Innovation isn't a one-time event; it's a culture. It's about daring to question the status quo, nurturing a curiosity that stretches horizons, and constantly seeking new ways to add value #Innovation #ChangeTheWorld")
 
+
+# Slack Testing Tool
+userid_dict = SlackTool().generate_userid_cum_name_dict()
+print(userid_dict)
+'''
+    The ids from the above dict can be used to send and retrieve messages from groups/ individual chats
+'''
+result = SlackReadMessageTool().execute({"id": "C059UFQPRU5"})
+print(result)
+result = SlackMessageTool().execute({"id": "U059QRHRYDU", "message": "Hii!"})
+print(result)
 
 
 superagi = SuperAgi.from_llm_and_tools("Super AGI", "To solve any complex problems for you", memory, tools, OpenAi(model="gpt-4"))
