9f4468ff72c10a5186e81279109769a2c1797338
NishantBorthakur <nist.16br@gmail.com>
2023-05-30 11:22:42 +0530

    api endpoint update

--- gui/app/DashboardService.js
+++ gui/app/DashboardService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000'; //for testing
-// const API_BASE_URL = 'http://localhost:8001';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8001';
 
 export const getOrganization = () => {
   return axios.get(`${API_BASE_URL}/organisations/get/1`);
