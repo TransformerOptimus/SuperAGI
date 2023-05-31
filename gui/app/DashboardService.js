9765888d338db23b29eb717a674a1fd61758d72f
NishantBorthakur <nist.16br@gmail.com>
2023-05-31 11:50:13 +0530

    minor fix

--- gui/app/DashboardService.js
+++ gui/app/DashboardService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8001';
-const API_BASE_URL = 'http://192.168.1.61:8001';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8001';
+// const API_BASE_URL = 'http://192.168.1.61:8001';
 
 export const baseUrl = () => {
   return API_BASE_URL;
