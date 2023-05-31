c885f924936c14f2826cffd0aaf3d59c7981c0e4
Fluder-Paradyne <121793617+Fluder-Paradyne@users.noreply.github.com>
2023-05-31 15:56:52 +0530

    health check temp uncommented

--- main.py
+++ main.py
@@ -245,9 +245,9 @@ session.close()
 
 
 # #Unprotected route
-# @app.get("/hello/{name}")
-# async def say_hello(name: str,):
-#     return {"message": f"Hello {name}"}
+@app.get("/hello/{name}")
+async def say_hello(name: str,):
+    return {"message": f"Hello {name}"}
 
 
 # # __________________TO RUN____________________________
