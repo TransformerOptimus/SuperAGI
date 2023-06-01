cb09381a9a93a62f03dcbcc800a49809906baaa0
Fluder-Paradyne <121793617+Fluder-Paradyne@users.noreply.github.com>
2023-06-01 11:36:40 +0530

    cros update

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ app.add_middleware(DBSessionMiddleware, db_url=db_url)
 origins = [
     "http://localhost:3001",
     "http://localhost:3000",
+    "https://app.superagi.com",
     # Add more origins if needed
 ]
 
