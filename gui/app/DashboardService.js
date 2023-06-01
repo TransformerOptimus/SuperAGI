08ad9515b7f9f6680971f60bb5bf97280c25a7e5
Fluder-Paradyne <121793617+Fluder-Paradyne@users.noreply.github.com>
2023-06-01 11:28:02 +0530

    api base url

--- gui/app/DashboardService.js
+++ gui/app/DashboardService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000'; //for testing
+const API_BASE_URL = 'https://app.superagi.com/api'; //for testing
 // const API_BASE_URL = 'http://localhost:8001';
 
 export const getOrganization = () => {
