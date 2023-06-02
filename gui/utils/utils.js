065f0217fbf6de30be324ae2f4551361b147ccf7
NishantBorthakur <nist.16br@gmail.com>
2023-06-02 11:48:26 +0530

    calls and tokens consumed

--- gui/utils/utils.js
+++ gui/utils/utils.js
@@ -15,3 +15,15 @@ export const formatTime = (lastExecutionTime) => {
     return 'Invalid Time';
   }
 };
+
+export const formatNumber = (number) => {
+  if (number === null || number === undefined || number === 0) {
+    return '0';
+  }
+
+  const suffixes = ['', 'k', 'M', 'B', 'T'];
+  const magnitude = Math.floor(Math.log10(number) / 3);
+  const scaledNumber = number / Math.pow(10, magnitude * 3);
+  const suffix = suffixes[magnitude];
+  return scaledNumber.toFixed(1) + suffix;
+}
