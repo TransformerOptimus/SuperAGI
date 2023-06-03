1086ad2e49a5f64367d17cf56b00c7d792ef53b4
NishantBorthakur <nist.16br@gmail.com>
2023-06-03 10:35:30 +0530

    resolving conflicts

---
+++ gui/app/DashboardService.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+const API_BASE_URL = 'http://192.168.170.48:8001'; //for testing
+// const API_BASE_URL = 'http://localhost:8001';
+
+export const getOrganization = () => {
+  return axios.get(`${API_BASE_URL}/organisations/get/1`);
+};
+
+export const addUser = (userData) => {
+  return axios.post(`${API_BASE_URL}/users/add`, userData);
+};
+
+export const getProject = (organisationId) => {
+  return axios.get(`${API_BASE_URL}/projects/get/organisation/${organisationId}`);
+};
+
+export const getAgents = (projectId) => {
+  return axios.get(`${API_BASE_URL}/agents/get/project/${projectId}`);
+};
+
+export const getAgent = (agentId) => {
+  return axios.get(`${API_BASE_URL}/agents/get/${agentId}`);
+};
+
+export const getTools = () => {
+  return axios.get(`${API_BASE_URL}/tools/get`);
+};
+
+export const getAgentDetails = (agentId) => {
+  return axios.get(`${API_BASE_URL}/agents/get/details/${agentId}`);
+};
+
+export const getAgentExecutions = (agentId) => {
+  return axios.get(`${API_BASE_URL}/agentexecutions/get/agent/${agentId}`);
+};
+
+export const getExecutionFeeds = (executionId) => {
+  return axios.get(`${API_BASE_URL}/agentexecutionfeeds/get/execution/${executionId}`);
+};
+
+export const createAgent = (agentData) => {
+  return axios.post(`${API_BASE_URL}/agents/create`, agentData);
+};
+
+export const updateAgents = (agentData) => {
+  return axios.put(`${API_BASE_URL}/agentconfigs/update/`, agentData);
+};
+
+export const updateExecution = (executionId, executionData) => {
+  return axios.put(`${API_BASE_URL}/agentexecutions/update/${executionId}`, executionData);
+};
+
+export const addExecution = (executionData) => {
+  return axios.post(`${API_BASE_URL}/agentexecutions/add`, executionData);
+};
