2ddb038a3918b25c333b806158ee52628a1e2a92
COLONAYUSH <ayushkaps9462@gmail.com>
2023-06-05 17:53:10 +0530

    Added click element by id tool for web interaction

---
+++ superagi/tools/web_Interactor/click_element_by_id.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, Field
+from superagi.helper.browser_wrapper import browser_wrapper
+from superagi.tools.base_tool import BaseTool
+
+black_listed_elements = set(["html", "head", "title", "meta", "iframe", "body", "style", "script", "path", "svg", "br", "::marker",])
+# global page_element_buffer
+# page_element_buffer = {}
+
+
+class ClickElementByIdSchema(BaseModel):
+    element_id: str = Field(..., description="The ID of the HTML element to click.")
+
+
+class ClickElementByIdTool(BaseTool):
+    name = "Click Element By ID"
+    description = "A tool for clicking an element by its ID using Playwright.clicks an element. Specify the id with the unique id received from the get_dom command. CRITICAL: The ID must be the integer id from the get_dom command. It should execute after getting the DOM"
+    args_schema = ClickElementByIdSchema
+
+    def _execute(self, element_id: str) -> str:
+        page_element_buffer = browser_wrapper.page_element_buffer
+        client = browser_wrapper.client
+        page = browser_wrapper.page
+
+        # Inject javascript into the page which removes the target= attribute from all links
+        js = """
+        links = document.getElementsByTagName("a");
+        for (var i = 0; i < links.length; i++) {
+            links[i].removeAttribute("target");
+        }
+        """
+        page.evaluate(js)
+
+        element = page_element_buffer.get(int(element_id))
+        if element:
+            x = element.get("center_x")
+            y = element.get("center_y")
+
+            page.mouse.click(x, y)
+        else:
+            return "Could not find element"
+
+        return "Successfully clicked!"
