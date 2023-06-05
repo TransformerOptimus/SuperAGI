869e95df431443a8ac6a2cde155c0e80c9648db5
COLONAYUSH <ayushkaps9462@gmail.com>
2023-06-05 17:49:37 +0530

    Added go to website tool logic for web interaction

---
+++ superagi/tools/web_Interactor/go_to_website.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from superagi.tools.base_tool import BaseTool
+from superagi.helper.browser_wrapper import browser_wrapper
+
+class GoToWebsiteSchema(BaseModel):
+    url: str = Field(..., description="The URL to navigate to.")
+
+
+class GoToWebsiteTool(BaseTool):
+    name = "Go To Website"
+    description = (
+        "A tool for navigation to a specified URL using the Playwright browser.Goes to a website in the web interaction plugin. Must be ran after starting the browser and before attempting to interact with a website"
+    )
+    args_schema = GoToWebsiteSchema
+
+    def _execute(self, url: str) -> str:
+        page = browser_wrapper.page
+        client = browser_wrapper.client
+
+        try:
+            page.goto(url=url if "://" in url else "http://" + url)
+            client = page.context.new_cdp_session(page)
+        except:
+            return "Failed to go to URL, please try again and make sure the URL is correct."
+
+        return "Navigated to URL " + url
