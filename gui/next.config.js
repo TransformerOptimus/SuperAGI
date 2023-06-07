225e0bfc928c023d4d0f83b752e27264c797b22b
Nihir Agarwal <nihir@contlo.com>
2023-06-07 20:02:50 +0530

    changes to route config

--- gui/next.config.js
+++ gui/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     assetPrefix: process.env.NODE_ENV === "production" ? "/" : "./",
+    env: {
+        NEXT_PUBLIC_GITHUB_CLIENT_ID : process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
+    },
     webpackDevMiddleware: config => {
     config.watchOptions = {
       poll: 1000,
