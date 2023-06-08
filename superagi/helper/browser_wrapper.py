17b6776a21ba35e35d2ab7bcf984ac11f852970e
rakesh-krishna-a-s <akrishna@contlo.com>
2023-06-08 13:44:08 +0530

    added vnc

--- superagi/helper/browser_wrapper.py
+++ superagi/helper/browser_wrapper.py
@@ -14,6 +14,7 @@ class BrowserWrapper:
             .start()
             .chromium.launch(
                 headless=False,
+                args=["--remote-debugging-port=9222"]
             )
         )
 
