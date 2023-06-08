2b464d5f9a1457989f468691d5d8896c34914e83
abhijeet <abhijeet@contlo.com>
2023-06-08 19:45:06 +0530

    Refactored and fixed S3 upload for write,image

--- superagi/helper/resource_helper.py
+++ superagi/helper/resource_helper.py
@@ -4,25 +4,17 @@ import os
 import datetime
 
 
-
-
-def make_written_file_resource(file_name: str, agent_id: int,file,channel):
+def make_written_file_resource(file_name: str, agent_id: int, file, channel):
     path = get_config("RESOURCES_OUTPUT_ROOT_DIR")
     storage_type = get_config("STORAGE_TYPE")
     file_extension = os.path.splitext(file_name)[1][1:]
 
     if file_extension in ["png", "jpg", "jpeg"]:
-        file_type = "image/"+ file_extension
+        file_type = "image/" + file_extension
     elif file_extension == "txt":
         file_type = "application/txt"
     else:
         file_type = "application/misc"
-
-
-    # print("______________MAKE FILE RESOURCE : ______________")
-    # print("FILE NAME : ",file_name)
-    # print("AGENT ID ", agent_id)
-    # print(file.name)
 
     root_dir = get_config('RESOURCES_OUTPUT_ROOT_DIR')
 
@@ -33,13 +25,7 @@ def make_written_file_resource(file_name: str, agent_id: int,file,channel):
     else:
         final_path = os.getcwd() + "/" + file_name
 
-    # print("FINAL PATH ")
-    # print(final_path)
-
     file_size = os.path.getsize(final_path)
-
-    # print("SIZE : ",file_size)
-
 
     if storage_type == "S3":
         file_name_parts = file_name.split('.')
@@ -50,11 +36,7 @@ def make_written_file_resource(file_name: str, agent_id: int,file,channel):
         else:
             path = 'output'
 
-    # print("_____________RES__________________")
-    # print(storage_type)
-    # print(file_name)
     print(path + "/" + file_name)
-    # print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     resource = Resource(name=file_name, path=path + "/" + file_name, storage_type=storage_type, size=file_size,
                         type=file_type,
                         channel="OUTPUT",
