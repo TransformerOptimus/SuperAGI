4abccc8046e780f58f6d2144fdb1b0cb011adaff
I’m <133493246+TransformerOptimus@users.noreply.github.com>
2023-06-08 16:22:58 +0530

    Removing unused function

--- superagi/tools/slack/tool.py
+++ superagi/tools/slack/tool.py
@@ -15,21 +15,5 @@ class SlackTool(BaseTool):
         slack_bot_token = get_config("SLACK_BOT_TOKEN")
         return WebClient(token=slack_bot_token)
     
-    def generate_userid_cum_name_dict(self):
-        slack = self.build_slack_web_client()
-        response_conversations_list, response_users_list = slack.conversations_list(), slack.users_list()
-        slack_id_name_dict = {'channels':{}, 'users': {}}
-        if response_conversations_list['ok']:
-            channels = response_conversations_list['channels']
-            for channel in channels:
-                slack_id_name_dict['channels'][channel['id']] = channel['name']
-                
-        if response_users_list['ok']:
-            members = response_users_list['members']
-            for member in members:
-                slack_id_name_dict['users'][member['id']] = member['name']
-        
-        return slack_id_name_dict
-    
     def _execute(self, *args: Any, **kwargs: Any):
         return super()._execute(*args, **kwargs)
