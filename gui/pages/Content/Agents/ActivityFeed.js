4ef9b31c851a8140f0dc483535709a18992501bf
NishantBorthakur <nist.16br@gmail.com>
2023-06-09 16:45:04 +0530

    back to top button

--- gui/pages/Content/Agents/ActivityFeed.js
+++ gui/pages/Content/Agents/ActivityFeed.js
@@ -5,12 +5,13 @@ import Image from "next/image";
 import {formatTime} from "@/utils/utils";
 import {EventBus} from "@/utils/eventBus";
 
-export default function ActivityFeed({selectedRunId}) {
+export default function ActivityFeed({selectedRunId, selectedView}) {
   const [loadingText, setLoadingText] = useState("Thinking");
   const [feeds, setFeeds] = useState([]);
   const feedContainerRef = useRef(null);
   const [runStatus, setRunStatus] = useState("CREATED");
   const [prevFeedsLength, setPrevFeedsLength] = useState(0);
+  const sideBarOpen = selectedView !== '' || false;
 
   useEffect(() => {
     const text = 'Thinking';
@@ -42,6 +43,14 @@ export default function ActivityFeed({selectedRunId}) {
       }
     }
   }, [feeds]);
+
+  function scrollToTop() {
+    if (feedContainerRef.current) {
+      setTimeout(() => {
+        feedContainerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+      }, 100);
+    }
+  }
 
   useEffect(() => {
     fetchFeeds();
@@ -78,8 +87,8 @@ export default function ActivityFeed({selectedRunId}) {
   });
 
   return (<>
-    <div style={{overflowY: "auto",maxHeight:'80vh'}} ref={feedContainerRef}>
-      <div style={{marginBottom:'140px'}}>
+    <div style={{overflowY: "auto",maxHeight:'80vh',position:'relative'}} ref={feedContainerRef}>
+      <div style={{marginBottom:'55px'}}>
         {feeds && feeds.map((f, index) => (<div key={index} className={styles.history_box} style={{background:'#272335',padding:'20px',cursor:'default'}}>
           <div style={{display:'flex'}}>
             {f.role === 'user' && <div className={styles.feed_icon}>💁</div>}
@@ -119,6 +128,10 @@ export default function ActivityFeed({selectedRunId}) {
           </div>
         </div>}
       </div>
+      {feedContainerRef.current && feedContainerRef.current.scrollTop >= 1200 &&
+        <div className="back_to_top" onClick={scrollToTop} style={sideBarOpen ? {right:'calc(39% - 5vw)'} : {right:'39%'}}>
+        <Image width={15} height={15} src="/images/backtotop.svg" alt="back-to-top"/>
+      </div>}
     </div>
   </>)
 }
