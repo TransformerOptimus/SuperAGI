1bb756642b4fe3f9718c8d3a9a2478d87f4a91fe
AdityaRajSingh1992 <105219157+AdityaRajSingh1992@users.noreply.github.com>
2023-06-09 15:53:00 +0530

    Create knowledge_tool.py

---
+++ superagi/helper/knowledge_tool.py
@@ -0,0 +1,22 @@
+  def get_search_index(self):
+    embed_model = "text-embedding-ada-002"
+    index_name = PineconeSearchModel.search_index_name
+    # pinecone.init(api_key="b5023255-b521-4ba6-aefc-f55d65f5a59b", enviroment="us-east-1-aws")
+    if index_name not in pinecone.list_indexes():
+      openai.api_key = os.getenv("OPENAI_API_KEY")
+      res = openai.Embedding.create(
+        input=[
+          "Sample document text goes here",
+          "there will be several phrases in each batch"
+        ], engine=embed_model
+      )
+
+      # if does not exist, create index
+      pinecone.create_index(
+        index_name,
+        dimension=len(res['data'][0]['embedding']),
+        metric='dotproduct'
+      )
+    # # connect to index
+    index = pinecone.Index(index_name)
+    return index
