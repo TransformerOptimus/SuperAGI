59e5bf40512e9224bebab84abdc816045a748a6d
COLONAYUSH <ayushkaps9462@gmail.com>
2023-06-13 19:29:01 +0530

    Modularise the get_dom tool

--- superagi/tools/web_interactor/playwright_get_dom.py
+++ superagi/tools/web_interactor/playwright_get_dom.py
@@ -16,54 +16,81 @@ from sys import argv, exit, platform
 black_listed_elements = set(["html", "head", "title", "meta", "iframe", "body", "style", "script", "path", "svg", "br", "::marker",])
 
 class GetDOMSchema(BaseModel):
-    url: str = Field(
-        ..., 
-        description="The URL of the page whose DOM needs to be extracted"
-        )
+    url: str = Field(..., description="The URL of the page whose DOM needs to be extracted")
 
 
 class GetDOMTool(BaseTool):
     name = "Get DOM"
     description = "A tool to retrieve the rendered DOM from the current page.Returns a simplified DOM of the current web page. The id is specific to this plugin and will be needed to interact with elements. Make sure to run this before interacting with any elements on a webpage. Re-run this each time you're on a new webpage and want to interact with elements"
-    args_schema: Type[GetDOMSchema] = GetDOMSchema
+    args_schema = GetDOMSchema
 
     def _execute(self) -> str:
-        page = browser_wrapper.page
-        page_element_buffer = browser_wrapper.page_element_buffer
-        client = browser_wrapper.client
-        
+        page = self.browser_wrapper.page
+        page_element_buffer = self.browser_wrapper.page_element_buffer
+        client = self.browser_wrapper.client
+
         if page is None:
             return "Error: Browser/Page has not been initialized yet. Please run Start Browser and Go to Page Tool first."
+
+        self.print_page_information()
+        percentage_progress_start, percentage_progress_end = self.calculate_progress()
+
+        page_state_as_text = self.create_page_state_as_text(percentage_progress_start, percentage_progress_end)
+
+        tree = self.capture_dom_snapshot()
+        strings, document, nodes = self.extract_tree_components(tree)
+
+        anchor_ancestry = {"-1": (False, None)}
+        button_ancestry = {"-1": (False, None)}
+        child_nodes = {}
+
+        elements_in_view_port = self.gather_elements_in_view_port(nodes, strings, document, child_nodes, anchor_ancestry, button_ancestry)
+    
+        elements_of_interest = self.process_elements_of_interest(elements_in_view_port, child_nodes)
+    
+        if len(elements_of_interest) > 125:
+            divided_elements_of_interest = self.divide_elements_of_interest(elements_of_interest)
+            return repr(divided_elements_of_interest) + "This is not part of the DOM, note that not the entire DOM was returned as it exceeded the context limit. If you're sure what you need is not included in this DOM, you should find a workaround."
+
+        return repr(elements_of_interest)
+
+
+    def print_page_information(self):
+        page = self.browser_wrapper.page
+        page_element_buffer = self.browser_wrapper.page_element_buffer
         print("##################################     PAGE      ######################################")
         print(page)
         print()
         print("##################################     PAGE Buffer      ######################################")
         print(page_element_buffer)
         print("######################################     END    ##################################")
-        page_state_as_text = []
+
+
+    def calculate_progress(self):
+        page = self.browser_wrapper.page
         device_pixel_ratio = page.evaluate("window.devicePixelRatio")
         if platform == "darwin" and device_pixel_ratio == 1:  # lies
             device_pixel_ratio = 2
 
-        win_scroll_x        = page.evaluate("window.scrollX")
-        win_scroll_y        = page.evaluate("window.scrollY")
-        win_upper_bound     = page.evaluate("window.pageYOffset")
-        win_left_bound      = page.evaluate("window.pageXOffset") 
-        win_width           = page.evaluate("window.screen.width")
-        win_height          = page.evaluate("window.screen.height")
-        win_right_bound     = win_left_bound + win_width
-        win_lower_bound     = win_upper_bound + win_height
+        win_scroll_x = page.evaluate("window.scrollX")
+        win_scroll_y = page.evaluate("window.scrollY")
+        win_upper_bound = page.evaluate("window.pageYOffset")
+        win_left_bound = page.evaluate("window.pageXOffset")
+        win_width = page.evaluate("window.screen.width")
+        win_height = page.evaluate("window.screen.height")
+        win_right_bound = win_left_bound + win_width
+        win_lower_bound = win_upper_bound + win_height
         document_offset_height = page.evaluate("document.body.offsetHeight")
         document_scroll_height = page.evaluate("document.body.scrollHeight")
 
-    #       percentage_progress_start = (win_upper_bound / document_scroll_height) * 100
-    #       percentage_progress_end = (
-    #           (win_height + win_upper_bound) / document_scroll_height
-    #       ) * 100
         percentage_progress_start = 1
         percentage_progress_end = 2
 
-        page_state_as_text.append(
+        return percentage_progress_start, percentage_progress_end
+
+
+    def create_page_state_as_text(self, percentage_progress_start, percentage_progress_end):
+        return [
             {
                 "x": 0,
                 "y": 0,
@@ -71,133 +98,47 @@ class GetDOMTool(BaseTool):
                     round(percentage_progress_start, 2), round(percentage_progress_end)
                 ),
             }
-        )
+        ]
 
-        tree = client.send(
+
+    def capture_dom_snapshot(self):
+        client = self.browser_wrapper.client
+        return client.send(
             "DOMSnapshot.captureSnapshot",
             {"computedStyles": [], "includeDOMRects": True, "includePaintOrder": True},
         )
-        strings     = tree["strings"]
-        document    = tree["documents"][0]
-        nodes       = document["nodes"]
-        backend_node_id = nodes["backendNodeId"]
-        attributes  = nodes["attributes"]
-        node_value  = nodes["nodeValue"]
-        parent      = nodes["parentIndex"]
-        node_types  = nodes["nodeType"]
-        node_names  = nodes["nodeName"]
-        is_clickable = set(nodes["isClickable"]["index"])
 
-        text_value          = nodes["textValue"]
-        text_value_index    = text_value["index"]
-        text_value_values   = text_value["value"]
 
-        input_value         = nodes["inputValue"]
-        input_value_index   = input_value["index"]
-        input_value_values  = input_value["value"]
+    def extract_tree_components(self, tree):
+        strings = tree["strings"]
+        document = tree["documents"][0]
+        nodes = document["nodes"]
+        return strings, document, nodes
 
-        input_checked       = nodes["inputChecked"]
-        layout              = document["layout"]
-        layout_node_index   = layout["nodeIndex"]
-        bounds              = layout["bounds"]
 
-        cursor = 0
-        html_elements_text = []
-
-        child_nodes = {}
+    def gather_elements_in_view_port(self, nodes, strings, document, child_nodes, anchor_ancestry, button_ancestry):
         elements_in_view_port = []
 
-        anchor_ancestry = {"-1": (False, None)}
-        button_ancestry = {"-1": (False, None)}
-
-        def convert_name(node_name, has_click_handler):
-            if node_name == "a":
-                return "link"
-            if node_name == "input" or node_name == "textarea":
-                return "input"
-            if node_name == "img":
-                return "img"
-            if (
-                node_name == "button" or has_click_handler
-            ):  # found pages that needed this quirk
-                return "button"
-            else:
-                return "text"
-
-        def find_attributes(attributes, keys):
-            values = {}
-
-            for [key_index, value_index] in zip(*(iter(attributes),) * 2):
-                if value_index < 0:
-                    continue
-                key = strings[key_index]
-                value = strings[value_index]
-
-                if key in keys:
-                    values[key] = value
-                    keys.remove(key)
-
-                    if not keys:
-                        return values
-
-            return values
-
-        def add_to_hash_tree(hash_tree, tag, node_id, node_name, parent_id):
-            parent_id_str = str(parent_id)
-            if not parent_id_str in hash_tree:
-                parent_name = strings[node_names[parent_id]].lower()
-                grand_parent_id = parent[parent_id]
-
-                add_to_hash_tree(
-                    hash_tree, tag, parent_id, parent_name, grand_parent_id
-                )
-
-            is_parent_desc_anchor, anchor_id = hash_tree[parent_id_str]
-
-            # even if the anchor is nested in another anchor, we set the "root" for all descendants to be ::Self
-            if node_name == tag:
-                value = (True, node_id)
-            elif (
-                is_parent_desc_anchor
-            ):  # reuse the parent's anchor_id (which could be much higher in the tree)
-                value = (True, anchor_id)
-            else:
-                value = (
-                    False,
-                    None,
-                )  # not a descendant of an anchor, most likely it will become text, an interactive element or discarded
-
-            hash_tree[str(node_id)] = value
-
-            return value
-
-        for index, node_name_index in enumerate(node_names):
-            node_parent = parent[index]
+        for index, node_name_index in enumerate(nodes["nodeName"]):
+            node_parent = nodes["parentIndex"][index]
             node_name = strings[node_name_index].lower()
 
-            is_ancestor_of_anchor, anchor_id = add_to_hash_tree(
-                anchor_ancestry, "a", index, node_name, node_parent
-            )
-
-            is_ancestor_of_button, button_id = add_to_hash_tree(
-                button_ancestry, "button", index, node_name, node_parent
-            )
+            is_ancestor_of_anchor, anchor_id = self.add_to_hash_tree(anchor_ancestry, "a", index, node_name, node_parent)
+            is_ancestor_of_button, button_id = self.add_to_hash_tree(button_ancestry, "button", index, node_name, node_parent)
 
             try:
-                cursor = layout_node_index.index(
-                    index
-                )  # todo replace this with proper cursoring, ignoring the fact this is O(n^2) for the moment
+                cursor = document["layout"]["nodeIndex"].index(index)
             except:
                 continue
 
             if node_name in black_listed_elements:
                 continue
 
-            [x, y, width, height] = bounds[cursor]
-            x /= device_pixel_ratio
-            y /= device_pixel_ratio
-            width /= device_pixel_ratio
-            height /= device_pixel_ratio
+            [x, y, width, height] = document["layout"]["bounds"][cursor]
+            x /= self.device_pixel_ratio
+            y /= self.device_pixel_ratio
+            width /= self.device_pixel_ratio
+            height /= self.device_pixel_ratio
 
             elem_left_bound = x
             elem_top_bound = y
@@ -205,10 +146,10 @@ class GetDOMTool(BaseTool):
             elem_lower_bound = y + height
 
             partially_is_in_viewport = (
-                elem_left_bound < win_right_bound
-                and elem_right_bound >= win_left_bound
-                and elem_top_bound < win_lower_bound
-                and elem_lower_bound >= win_upper_bound
+                elem_left_bound < self.win_right_bound
+                and elem_right_bound >= self.win_left_bound
+                and elem_top_bound < self.win_lower_bound
+                and elem_lower_bound >= self.win_upper_bound
             )
 
             if not partially_is_in_viewport:
@@ -216,9 +157,8 @@ class GetDOMTool(BaseTool):
 
             meta_data = []
 
-            # inefficient to grab the same set of keys for kinds of objects but its fine for now
-            element_attributes = find_attributes(
-                attributes[index], ["type", "placeholder", "aria-label", "title", "alt"]
+            element_attributes = self.find_attributes(
+                nodes["attributes"][index], ["type", "placeholder", "aria-label", "title", "alt"]
             )
 
             ancestor_exception = is_ancestor_of_anchor or is_ancestor_of_button
@@ -236,7 +176,7 @@ class GetDOMTool(BaseTool):
             )
 
             if node_name == "#text" and ancestor_exception:
-                text = strings[node_value[index]]
+                text = strings[nodes["nodeValue"][index]]
                 if text == "|" or text == "•":
                     continue
                 ancestor_node.append({
@@ -247,10 +187,8 @@ class GetDOMTool(BaseTool):
                     node_name == "input" and element_attributes.get("type") == "submit"
                 ) or node_name == "button":
                     node_name = "button"
-                    element_attributes.pop(
-                        "type", None
-                    )  # prevent [button ... (button)..]
-                
+                    element_attributes.pop("type", None)
+            
                 for key in element_attributes:
                     if ancestor_exception:
                         ancestor_node.append({
@@ -263,32 +201,31 @@ class GetDOMTool(BaseTool):
 
             element_node_value = None
 
-            if node_value[index] >= 0:
-                element_node_value = strings[node_value[index]]
-                if element_node_value == "|": #commonly used as a seperator, does not add much context - lets save ourselves some token space
+            if nodes["nodeValue"][index] >= 0:
+                element_node_value = strings[nodes["nodeValue"][index]]
+                if element_node_value == "|":
                     continue
             elif (
                 node_name == "input"
-                and index in input_value_index
+                and index in nodes["inputValue"]["index"]
                 and element_node_value is None
             ):
-                node_input_text_index = input_value_index.index(index)
-                text_index = input_value_values[node_input_text_index]
+                node_input_text_index = nodes["inputValue"]["index"].index(index)
+                text_index = nodes["inputValue"]["value"][node_input_text_index]
                 if node_input_text_index >= 0 and text_index >= 0:
                     element_node_value = strings[text_index]
 
-            # remove redudant elements
             if ancestor_exception and (node_name != "a" and node_name != "button"):
                 continue
 
             elements_in_view_port.append(
                 {
                     "node_index": str(index),
-                    "backend_node_id": backend_node_id[index],
+                    "backend_node_id": nodes["backendNodeId"][index],
                     "node_name": node_name,
                     "node_value": element_node_value,
                     "node_meta": meta_data,
-                    "is_clickable": index in is_clickable,
+                    "is_clickable": index in set(nodes["isClickable"]["index"]),
                     "origin_x": int(x),
                     "origin_y": int(y),
                     "center_x": int(x + (width / 2)),
@@ -296,9 +233,12 @@ class GetDOMTool(BaseTool):
                 }
             )
 
-        # lets filter further to remove anything that does not hold any text nor has click handlers + merge text from leaf#text nodes with the parent
-        elements_of_interest= []
-        id_counter          = 0
+        return elements_in_view_port
+
+
+    def process_elements_of_interest(self, elements_in_view_port, child_nodes):
+        elements_of_interest = []
+        id_counter = 0
 
         for element in elements_in_view_port:
             node_index = element.get("node_index")
@@ -313,14 +253,14 @@ class GetDOMTool(BaseTool):
 
             inner_text = f"{node_value} " if node_value else ""
             meta = ""
-            
+
             if node_index in child_nodes:
                 for child in child_nodes.get(node_index):
-                    entry_type = child.get('type')
-                    entry_value= child.get('value')
+                    entry_type = child.get("type")
+                    entry_value = child.get("value")
 
                     if entry_type == "attribute":
-                        entry_key = child.get('key')
+                        entry_key = child.get("key")
                         meta_data.append(f'{entry_key}="{entry_value}"')
                     else:
                         inner_text += f"{entry_value} "
@@ -332,9 +272,8 @@ class GetDOMTool(BaseTool):
             if inner_text != "":
                 inner_text = f"{inner_text.strip()}"
 
-            converted_node_name = convert_name(node_name, is_clickable)
+            converted_node_name = self.convert_name(node_name, is_clickable)
 
-            # not very elegant, more like a placeholder
             if (
                 (converted_node_name != "button" or meta == "")
                 and converted_node_name != "link"
@@ -344,25 +283,27 @@ class GetDOMTool(BaseTool):
             ) and inner_text.strip() == "":
                 continue
 
-            page_element_buffer[id_counter] = element
+            self.page_element_buffer[id_counter] = element
 
-            if inner_text != "": 
+            if inner_text != "":
                 elements_of_interest.append(
-                    f"""<{converted_node_name} id={id_counter}{meta}>{inner_text}</{converted_node_name}>"""
+                    f"<{converted_node_name} id={id_counter}{meta}>{inner_text}</{converted_node_name}>"
                 )
             else:
                 elements_of_interest.append(
-                    f"""<{converted_node_name} id={id_counter}{meta}/>"""
+                    f"<{converted_node_name} id={id_counter}{meta}/>"
                 )
             id_counter += 1
 
-        if len(elements_of_interest) > 125:
-            idCounter =0
-            divided_elements_of_interest = []
-            while idCounter <= 125:
-                divided_elements_of_interest.append(elements_of_interest[idCounter])
-                idCounter+=1
+        return elements_of_interest
 
-            return repr(divided_elements_of_interest) + "This is not part of the DOM, note that not the entire DOM was returned as it exceeded the context limit. If you're sure what you need is not included in this DOM, you should find a workaround."
-        
-        return repr(elements_of_interest)
+
+    def divide_elements_of_interest(self, elements_of_interest):
+        id_counter = 0
+        divided_elements_of_interest = []
+
+        while id_counter <= 125:
+            divided_elements_of_interest.append(elements_of_interest[id_counter])
+            id_counter += 1
+
+        return divided_elements_of_interest
