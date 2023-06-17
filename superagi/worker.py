ac9cead9490cd5708f38b73595dbb04ac9566b73
rakesh-krishna-a-s <akrishna@contlo.com>
2023-06-17 13:21:17 +0530

    added test test_handle_wait_for_permission.py
    fixed small error where in agent_execution_feed.py had type instead of role in its __repr__

--- superagi/worker.py
+++ superagi/worker.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from celery import Celery
 
 from superagi.config.config import get_config
-from superagi.jobs.agent_executor import AgentExecutor
 redis_url = get_config('REDIS_URL')
 
 app = Celery("superagi", include=["superagi.worker"], imports=["superagi.worker"])
@@ -14,5 +13,6 @@ app.conf.worker_concurrency = 10
 @app.task(name="execute_agent", autoretry_for=(Exception,), retry_backoff=2, max_retries=5)
 def execute_agent(agent_execution_id: int, time):
     """Execute an agent step in background."""
+    from superagi.jobs.agent_executor import AgentExecutor
     print("Execute agent:" + str(time) + "," + str(agent_execution_id))
     AgentExecutor().execute_next_action(agent_execution_id=agent_execution_id)
