4d141042b22e1107a250f267367119448d9d3fa4
NishantBorthakur <nist.16br@gmail.com>
2023-06-19 11:40:03 +0530

    marketplace fixes

--- gui/pages/Content/Marketplace/MarketAgent.js
+++ gui/pages/Content/Marketplace/MarketAgent.js
@@ -24,22 +24,19 @@ export default function MarketAgent(){
 
     return (
         <div>
-    {/*<div className={styles.history_box}>*/}
-    {/*    Agents*/}
-    {/*</div>*/}
-             <div className={styles.rowContainer} style={{maxHeight: '78vh',overflowY: 'auto'}}>
-                 <div className='row' style={{paddingLeft:'2.5%'}}>
+           <div className={styles.rowContainer} style={{maxHeight: '78vh',overflowY: 'auto'}}>
+             <div className='row' style={{paddingLeft:'2.5%'}}>
                 {agentTemplates.map((item, index) => (
-                    <div className={styles.market_tool} key={item.id} style={{cursor: 'pointer'}}  onClick={() => handleTemplateClick(item)}>
-                        <div style={{display: 'inline',overflow:'auto'}}>
-                            <div>{item.name}</div>
-                            <div style={{marginTop: '2%',lineHeight: '14px', color: 'rgb(96, 96, 96)'}}>by SuperAgi&nbsp;<Image width={14} height={14} src="/images/is_verified.svg" alt="is_verified"/></div>
-                            <div style={{lineHeight: '14px', color: 'rgb(96, 96, 96)',wordWrap: 'break-word',overflowWrap: 'break-word'}}>{item.description}</div>
-                        </div>
+                  <div className={styles.market_tool} key={item.id} style={{cursor: 'pointer'}}  onClick={() => handleTemplateClick(item)}>
+                    <div style={{display: 'inline',overflow:'auto'}}>
+                      <div>{item.name}</div>
+                      <div style={{marginTop: '2%',lineHeight: '14px', color: 'rgb(96, 96, 96)'}}>by SuperAgi&nbsp;<Image width={14} height={14} src="/images/is_verified.svg" alt="is_verified"/></div>
+                      <div style={{lineHeight: '14px', color: 'rgb(96, 96, 96)',wordWrap: 'break-word',overflowWrap: 'break-word'}}>{item.description}</div>
                     </div>
+                  </div>
                 ))}
-                 </div>
-            </div>
+             </div>
+          </div>
     </div>
     )  
 };
