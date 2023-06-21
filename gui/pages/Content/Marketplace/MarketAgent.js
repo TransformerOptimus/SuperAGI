5dd694d3d86a74e2fa40e6019e920f261e18bf4c
Kalki <parth.pathak@science.christuniversity.in>
2023-06-21 18:17:09 +0530

    ui changes

--- gui/pages/Content/Marketplace/MarketAgent.js
+++ gui/pages/Content/Marketplace/MarketAgent.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import styles from './Market.module.css';
 import {fetchAgentTemplateList} from "@/pages/api/DashboardService";
 import {EventBus} from "@/utils/eventBus";
-import {loadingTextEffect} from "@/utils/utils";
 
 export default function MarketAgent(){
     const [agentTemplates, setAgentTemplates] = useState([])
@@ -12,21 +11,30 @@ export default function MarketAgent(){
     const [loadingText, setLoadingText] = useState("Loading Templates");
 
     useEffect(() => {
-      loadingTextEffect('Loading Templates', setLoadingText, 500);
+        const text = 'Loading Templates';
+        let dots = '';
 
-      if(window.location.href.toLowerCase().includes('marketplace')) {
-          setShowMarketplace(true)
-      }
+        const interval = setInterval(() => {
+            dots = dots.length < 3 ? dots + '.' : '';
+            setLoadingText(`${text}${dots}`);
+        }, 500);
 
-      fetchAgentTemplateList()
-        .then((response) => {
-            const data = response.data || [];
-            setAgentTemplates(data);
-            setIsLoading(false);
-        })
-        .catch((error) => {
-            console.error('Error fetching agent templates:', error);
-        });
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        if(window.location.href.toLowerCase().includes('marketplace')) {
+            setShowMarketplace(true)
+        }
+        fetchAgentTemplateList()
+            .then((response) => {
+                const data = response.data || [];
+                setAgentTemplates(data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching agent templates:', error);
+            });
     }, []);
 
     function handleTemplateClick(item) {
@@ -35,21 +43,21 @@ export default function MarketAgent(){
 
     return (
         <div style={showMarketplace ? { marginLeft:'8px',marginRight:'8px' } : { marginLeft:'3px' }}>
-           <div className={styles.rowContainer} style={{maxHeight: '78vh',overflowY: 'auto'}}>
-             {!isLoading ? <div className={styles.resources}>
-                {agentTemplates.map((item, index) => (
-                  <div className={styles.market_tool} key={item.id} style={{cursor: 'pointer'}}  onClick={() => handleTemplateClick(item)}>
-                    <div style={{display: 'inline',overflow:'auto'}}>
-                      <div>{item.name}</div>
-                      <div style={{color: '#888888',lineHeight:'16px'}}>by SuperAgi&nbsp;<Image width={14} height={14} src="/images/is_verified.svg" alt="is_verified"/></div>
-                      <div className={styles.tool_description}>{item.description}</div>
-                    </div>
-                  </div>
-                ))}
-             </div> : <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'75vh'}}>
-             <div className="signInInfo" style={{fontSize:'16px',fontFamily:'Source Code Pro'}}>{loadingText}</div>
-           </div>}
-          </div>
-    </div>
-    )  
+            <div className={styles.rowContainer} style={{maxHeight: '78vh',overflowY: 'auto'}}>
+                {!isLoading ? <div className={styles.resources} style={agentTemplates.length === 2 ? {justifyContent:'flex-start',gap:'7px'} : {}}>
+                    {agentTemplates.map((item, index) => (
+                        <div className={styles.market_tool} key={item.id} style={{cursor: 'pointer'}}  onClick={() => handleTemplateClick(item)}>
+                            <div style={{display: 'inline',overflow:'auto'}}>
+                                <div>{item.name}</div>
+                                <div style={{color: '#888888',lineHeight:'16px'}}>by SuperAgi&nbsp;<Image width={14} height={14} src="/images/is_verified.svg" alt="is_verified"/></div>
+                                <div className={styles.tool_description}>{item.description}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div> : <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'75vh'}}>
+                    <div className="signInInfo" style={{fontSize:'16px',fontFamily:'Source Code Pro'}}>{loadingText}</div>
+                </div>}
+            </div>
+        </div>
+    )
 };
