00f60d0f6ab8e47db4bfabfc260374d6f579c466
Kalki <parth.pathak@science.christuniversity.in>
2023-06-22 12:13:13 +0530

    ui changes

--- gui/pages/Content/Agents/AgentWorkspace.js
+++ gui/pages/Content/Agents/AgentWorkspace.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useMemo} from 'react';
 import Image from 'next/image';
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
