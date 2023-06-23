8b70bd3031c3e390539383a20a291690b8d0005a
abhijeet <abhijeet@contlo.com>
2023-06-23 19:14:26 +0530

    adding of empty state for readme

--- gui/pages/Content/Marketplace/EachTool.js
+++ gui/pages/Content/Marketplace/EachTool.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Image from "next/image";
 import styles from '../Tools/Tool.module.css';
+import styles3 from '../Agents/Agents.module.css';
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import styles2 from "./Market.module.css"
@@ -14,8 +15,8 @@ export default function EachTool({template, env}) {
     const [markdownContent, setMarkdownContent] = useState(null);
 
     useEffect(() => {
-        setInstalled(template && template.is_installed? 'Installed' : 'Install');
-        if(window.location.href.toLowerCase().includes('marketplace')) {
+        setInstalled(template && template.is_installed ? 'Installed' : 'Install');
+        if (window.location.href.toLowerCase().includes('marketplace')) {
             setInstalled('Sign in to install')
         }
         fetchToolTemplateOverview(template.name)
@@ -29,8 +30,8 @@ export default function EachTool({template, env}) {
     }, []);
 
 
-    function handleInstallClick(){
-        if(window.location.href.toLowerCase().includes('marketplace')) {
+    function handleInstallClick() {
+        if (window.location.href.toLowerCase().includes('marketplace')) {
             if (env === 'PROD') {
                 window.open(`https://app.superagi.com/`, '_self');
             } else {
@@ -39,7 +40,7 @@ export default function EachTool({template, env}) {
             return;
         }
 
-        if(template && template.is_installed) {
+        if (template && template.is_installed) {
             toast.error("Template is already installed", {autoClose: 1800});
             return;
         }
@@ -54,65 +55,95 @@ export default function EachTool({template, env}) {
             });
     }
 
-    function handleBackClick(){
+    function handleBackClick() {
         EventBus.emit('goToMarketplace', {});
     }
 
     return (
         <>
             <div>
-                <div className="row" style={{marginLeft:'auto'}}>
-                    <div className={styles2.back_button} style={{margin: '8px 0',padding: '2px'}} onClick={() => handleBackClick()}>
+                <div className="row" style={{marginLeft: 'auto'}}>
+                    <div className={styles2.back_button} style={{margin: '8px 0', padding: '2px'}}
+                         onClick={() => handleBackClick()}>
                         <Image src="/images/arrow_back.svg" alt="back_button" width={14} height={12}/>
                         <span className={styles2.back_button_text}>Back</span>
                     </div>
-                    <div className="col-3" style={{maxHeight:'84vh',overflowY:'auto',padding:'0'}}>
+                    <div className="col-3" style={{maxHeight: '84vh', overflowY: 'auto', padding: '0'}}>
                         <div className={styles2.left_container}>
-                            <div style={{marginBottom:'15px'}}>
-                                <Image style={{borderRadius: '25px',background:'black'}} width={50} height={50} src="/images/app-logo-light.png" alt="tool-icon"/>
+                            <div style={{marginBottom: '15px'}}>
+                                <Image style={{borderRadius: '25px', background: 'black'}} width={50} height={50}
+                                       src="/images/app-logo-light.png" alt="tool-icon"/>
                             </div>
                             <span className={styles2.top_heading}>{template.name}</span>
-                            <span style={{fontSize: '12px',marginTop: '15px',}} className={styles.tool_publisher}>By SuperAGI <Image width={14} height={14} src="/images/is_verified.svg" alt="is_verified"/>&nbsp;{'\u00B7'}&nbsp;<Image width={14} height={14} src="/images/upload_icon.svg" alt="upload-icon"/></span>
-                            <button className="primary_button" style={{marginTop:'15px',width:'100%',background:template && template.is_installed ? 'rgba(255, 255, 255, 0.14)':'#FFF',color:template && template.is_installed ? '#FFFFFF':'#000'}} onClick={() => handleInstallClick()}>
-                                {(template && template.is_installed) ? <Image width={14} height={14} src="/images/tick.svg" alt="tick-icon"/> : <Image width={14} height={14} src="/images/upload_icon_dark.svg" alt="upload-icon"/>}&nbsp;{installed}</button>
-                            <hr className={styles2.horizontal_line} />
+                            <span style={{fontSize: '12px', marginTop: '15px',}} className={styles.tool_publisher}>By SuperAGI <Image
+                                width={14} height={14} src="/images/is_verified.svg"
+                                alt="is_verified"/>&nbsp;{'\u00B7'}&nbsp;<Image width={14} height={14}
+                                                                                src="/images/upload_icon.svg"
+                                                                                alt="upload-icon"/></span>
+                            <button className="primary_button" style={{
+                                marginTop: '15px',
+                                width: '100%',
+                                background: template && template.is_installed ? 'rgba(255, 255, 255, 0.14)' : '#FFF',
+                                color: template && template.is_installed ? '#FFFFFF' : '#000'
+                            }} onClick={() => handleInstallClick()}>
+                                {(template && template.is_installed) ?
+                                    <Image width={14} height={14} src="/images/tick.svg" alt="tick-icon"/> :
+                                    <Image width={14} height={14} src="/images/upload_icon_dark.svg"
+                                           alt="upload-icon"/>}&nbsp;{installed}</button>
+                            <hr className={styles2.horizontal_line}/>
                             <span className={styles2.description_text}>{template.description}</span>
-                            <hr className={styles2.horizontal_line} />
+                            <hr className={styles2.horizontal_line}/>
                             <span style={{fontSize: '12px',}} className={styles.tool_publisher}>Last updated</span>
                             <span className={styles2.description_text}>{template.updated_at}</span>
                         </div>
                     </div>
                     <div className="col-9" style={{paddingLeft: '8px'}}>
                         <div>
-                            <div className={styles2.left_container} style={{marginBottom:'5px',padding:'8px'}}>
+                            <div className={styles2.left_container} style={{marginBottom: '5px', padding: '8px'}}>
                                 <div className="row">
                                     <div className="col-4">
-                                        <button onClick={() => setRightPanel('overview')} className={styles2.tab_button} style={rightPanel === 'overview' ? {background:'#454254',paddingRight:'15px'} : {background:'transparent',paddingRight:'15px'}}>
+                                        <button onClick={() => setRightPanel('overview')} className={styles2.tab_button}
+                                                style={rightPanel === 'overview' ? {
+                                                    background: '#454254',
+                                                    paddingRight: '15px'
+                                                } : {background: 'transparent', paddingRight: '15px'}}>
                                             &nbsp;Overview
                                         </button>
-                                        <button onClick={() => setRightPanel('tool_view')} className={styles2.tab_button} style={rightPanel === 'tool_view' ? {background:'#454254',paddingRight:'15px'} : {background:'transparent',paddingRight:'15px'}}>
+                                        <button onClick={() => setRightPanel('tool_view')}
+                                                className={styles2.tab_button} style={rightPanel === 'tool_view' ? {
+                                            background: '#454254',
+                                            paddingRight: '15px'
+                                        } : {background: 'transparent', paddingRight: '15px'}}>
                                             &nbsp;Tools Included
                                         </button>
                                     </div>
                                 </div>
                             </div>
-                            {rightPanel === 'overview' && <div className={styles2.left_container} style={{marginBottom: '8px'}}>
-                                <div className={styles2.markdown_container}>
-                                    <ReactMarkdown className={styles2.markdown_style}>{markdownContent}</ReactMarkdown>
-                                </div>
-                                {/*<div>*/}
-                                {/*    <span className={styles2.description_heading} style={{fontWeight: '400'}}>{goals.length}&nbsp;Goals</span><br/><br/>*/}
-                                {/*    {goals.map((goal, index) => (<div key={index} style={{marginTop: '0'}}>*/}
-                                {/*        <div className={styles2.description_text}>{index + 1}. {goal || ''}</div>*/}
-                                {/*        {index !== goals.length - 1}*/}
-                                {/*    </div>))}*/}
-                                {/*</div>*/}
-                            </div>}
-                            {rightPanel==='tool_view' && <div>
-                                <div  style={{overflowY:'scroll',height:'70vh'}}>
+                            {rightPanel === 'overview' &&
+                                <div className={styles2.left_container} style={{marginBottom: '8px'}}>
+                                    <div className={styles2.markdown_container}>
+                                        {markdownContent ? <ReactMarkdown
+                                                className={styles2.markdown_style}>{markdownContent}</ReactMarkdown> :
+                                           <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',marginTop:'40px',width:'100%'}}>
+                                               <Image width={150} height={60} src="/images/no_permissions.svg" alt="no-permissions" />
+                                               <span className={styles3.feed_title} style={{marginTop: '8px'}}>No Overview to display!</span>
+                                           </div>
+                                        }
+                                    </div>
+                                    {/*<div>*/}
+                                    {/*    <span className={styles2.description_heading} style={{fontWeight: '400'}}>{goals.length}&nbsp;Goals</span><br/><br/>*/}
+                                    {/*    {goals.map((goal, index) => (<div key={index} style={{marginTop: '0'}}>*/}
+                                    {/*        <div className={styles2.description_text}>{index + 1}. {goal || ''}</div>*/}
+                                    {/*        {index !== goals.length - 1}*/}
+                                    {/*    </div>))}*/}
+                                    {/*</div>*/}
+                                </div>}
+                            {rightPanel === 'tool_view' && <div>
+                                <div style={{overflowY: 'scroll', height: '70vh'}}>
                                     {template.tools.map((value, index) => (
-                                        <div key={index} className={styles2.left_container} style={{marginBottom: '5px',color:'white',padding:'16px'}}>
-                                            <span className={styles2.description_text}>{value.name}</span><br />
+                                        <div key={index} className={styles2.left_container}
+                                             style={{marginBottom: '5px', color: 'white', padding: '16px'}}>
+                                            <span className={styles2.description_text}>{value.name}</span><br/>
                                             <span className={styles2.sub_text}>{value.description}</span>
                                         </div>
                                     ))}
