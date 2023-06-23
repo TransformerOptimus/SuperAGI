612a571bd784177886a1d11ba00b8f179f38d6d4
TransformerOptimus <muknrq@gmail.com>
2023-06-23 11:12:14 +0530

    moving agent prompts to file

--- superagi/helper/prompt_reader.py
+++ superagi/helper/prompt_reader.py
@@ -12,3 +12,14 @@ class PromptReader:
             print(e.__str__())
             raise e
         return file_content
+
+    @staticmethod
+    def read_agent_prompt(current_file: str, prompt_file: str) -> str:
+        file_path = str(Path(current_file).resolve().parent) + "/prompts/" + prompt_file
+        try:
+            f = open(file_path, "r")
+            file_content = f.read()
+        except FileNotFoundError as e:
+            print(e.__str__())
+            raise e
+        return file_content
