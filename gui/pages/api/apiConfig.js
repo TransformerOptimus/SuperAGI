f25798a5e831db3b430c63dbb67dd89161927015
NishantBorthakur <nist.16br@gmail.com>
2023-06-26 14:44:51 +0530

    toolkit addition

--- gui/pages/api/apiConfig.js
+++ gui/pages/api/apiConfig.js
@@ -3,12 +3,17 @@ import axios from 'axios';
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8001';
 
+
 export const baseUrl = () => {
   return API_BASE_URL;
 };
 
 export const githubClientId = () => {
   return GITHUB_CLIENT_ID;
+};
+
+export const googleClientId = () => {
+  return GOOGLE_CLIENT_ID;
 };
 
 const api = axios.create({
