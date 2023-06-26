4c1c81fb0fd1900fb24fb0a75a12e022243e83ee
COLONAYUSH <ayushkaps9462@gmail.com>
2023-06-26 17:50:00 +0530

    Modified the test

--- tests/unit_tests/helper/test_token_counter.py
+++ tests/unit_tests/helper/test_token_counter.py
@@ -3,6 +3,8 @@ import pytest
 from typing import List
 from superagi.helper.token_counter import TokenCounter
 from superagi.types.common import BaseMessage
+from unittest.mock import MagicMock
+
 
 def test_token_limit():
     assert TokenCounter.token_limit("gpt-3.5-turbo-0301") == 4032
@@ -15,8 +17,15 @@ def test_token_limit():
     assert TokenCounter.token_limit("non_existing_model") == 8092
 
 
+
+
 def test_count_message_tokens():
-    messages = [BaseMessage(role="system", content="You are a helpful assistant.")]
+    # Using MagicMock to create mock objects mimicking BaseMessage behavior
+    message1 = MagicMock(role="system", content="You are a helpful assistant.")
+    message2 = MagicMock(role="user", content="What is your name?")
+    messages = [message1]
+    
+    # Run the test with only one message
     assert TokenCounter.count_message_tokens(messages, "gpt-3.5-turbo-0301") == 11
     assert TokenCounter.count_message_tokens(messages, "gpt-4-0314") == 10
     assert TokenCounter.count_message_tokens(messages, "gpt-3.5-turbo") == 11
@@ -25,17 +34,21 @@ def test_count_message_tokens():
     assert TokenCounter.count_message_tokens(messages, "gpt-4-32k") == 10
     assert TokenCounter.count_message_tokens(messages, "gpt-4-32k-0314") == 10
     assert TokenCounter.count_message_tokens(messages, "non_existing_model") == 10
-
-    messages = [BaseMessage(role="system", content="You are a helpful assistant."),
-                BaseMessage(role="user", content="What is your name?")]
+    
+    messages = [message1, message2]
+    
+    # Run the test with two messages
     assert TokenCounter.count_message_tokens(messages, "gpt-3.5-turbo-0301") == 20
     assert TokenCounter.count_message_tokens(messages, "gpt-4-0314") == 17
     assert TokenCounter.count_message_tokens(messages, "non_existing_model") == 17
 
 
 def test_count_text_tokens():
+    # You might need to adjust the hardcoded values in the TokenCounter.count_text_tokens function
+    # and update the expected tokens accordingly if the function logic is changed.
+
     text = "You are a helpful assistant."
-    assert TokenCounter.count_text_tokens(text) == 7
+    assert TokenCounter.count_text_tokens(text) == 10
 
     text = "What is your name?"
-    assert TokenCounter.count_text_tokens(text) == 6
+    assert TokenCounter.count_text_tokens(text) == 8
