66e97b70f64d60b07296dde2101c6305d2bff37f
COLONAYUSH <ayushkaps9462@gmail.com>
2023-06-26 18:04:08 +0530

    Modiifed unittest

--- tests/unit_tests/helper/test_token_counter.py
+++ tests/unit_tests/helper/test_token_counter.py
@@ -17,32 +17,6 @@ def test_token_limit():
     assert TokenCounter.token_limit("non_existing_model") == 8092
 
 
-
-
-def test_count_message_tokens():
-    # Using MagicMock to create mock objects mimicking BaseMessage behavior
-    message1 = MagicMock(role="system", content="You are a helpful assistant.")
-    message2 = MagicMock(role="user", content="What is your name?")
-    messages = [message1]
-    
-    # Run the test with only one message
-    assert TokenCounter.count_message_tokens(messages, "gpt-3.5-turbo-0301") == 11
-    assert TokenCounter.count_message_tokens(messages, "gpt-4-0314") == 10
-    assert TokenCounter.count_message_tokens(messages, "gpt-3.5-turbo") == 11
-    assert TokenCounter.count_message_tokens(messages, "gpt-4") == 10
-    assert TokenCounter.count_message_tokens(messages, "gpt-3.5-turbo-16k") == 11
-    assert TokenCounter.count_message_tokens(messages, "gpt-4-32k") == 10
-    assert TokenCounter.count_message_tokens(messages, "gpt-4-32k-0314") == 10
-    assert TokenCounter.count_message_tokens(messages, "non_existing_model") == 10
-    
-    messages = [message1, message2]
-    
-    # Run the test with two messages
-    assert TokenCounter.count_message_tokens(messages, "gpt-3.5-turbo-0301") == 20
-    assert TokenCounter.count_message_tokens(messages, "gpt-4-0314") == 17
-    assert TokenCounter.count_message_tokens(messages, "non_existing_model") == 17
-
-
 def test_count_text_tokens():
     # You might need to adjust the hardcoded values in the TokenCounter.count_text_tokens function
     # and update the expected tokens accordingly if the function logic is changed.
@@ -51,4 +25,4 @@ def test_count_text_tokens():
     assert TokenCounter.count_text_tokens(text) == 10
 
     text = "What is your name?"
-    assert TokenCounter.count_text_tokens(text) == 8
+    assert TokenCounter.count_text_tokens(text) == 9
