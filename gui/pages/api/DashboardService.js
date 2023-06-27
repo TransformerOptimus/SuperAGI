ececcc0354b5bf73775e8ac99adbb201b50de01b
Tarraann <jot.taran15522@gmail.com>
2023-06-27 17:26:58 +0530

    Added api

--- gui/pages/api/DashboardService.js
+++ gui/pages/api/DashboardService.js
@@ -128,6 +128,10 @@ export const authenticateGoogleCred = (toolKitId) => {
   return api.get(`/google/get_google_creds/toolkit_id/${toolKitId}`);
 }
 
+export const authenticateTwitterCred = (toolKitId) => {
+  return api.get(`/twitter/get_twitter_creds/toolkit_id/${toolKitId}`);
+}
+
 export const fetchToolTemplateList = () => {
   return api.get(`/toolkits/get/list?page=0`);
 }
