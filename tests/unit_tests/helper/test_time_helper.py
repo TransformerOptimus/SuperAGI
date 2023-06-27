94e057bdb17a2985fa3e08ae789f596fc425587f
abhijeet <abhijeet@contlo.com>
2023-06-27 09:43:04 +0530

    added bug fix backend

---
+++ tests/unit_tests/helper/test_time_helper.py
@@ -0,0 +1,15 @@
+from superagi.helper.time_helper import get_time_difference
+
+
+def test_get_time_difference():
+    # Test case 1: Same timestamp, expect all time components to be zero
+    timestamp1 = "2023-06-26 17:31:08.884322"
+    timestamp2 = "2023-06-27 03:57:42.038497"
+    expected_result = {
+        "years": 0,
+        "months": 0,
+        "days": 0,
+        "hours": 10,
+        "minutes": 26
+    }
+    assert get_time_difference(timestamp1, timestamp2) == expected_result
