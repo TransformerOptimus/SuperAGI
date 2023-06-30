9eae73a466eed36d06c6cd5393e666c09e4e19dd
rakesh-krishna-a-s <akrishna@contlo.com>
2023-06-30 12:26:26 +0530

    closing file properly

--- superagi/helper/prompt_reader.py
+++ superagi/helper/prompt_reader.py
@@ -6,8 +6,8 @@ class PromptReader:
     def read_tools_prompt(current_file: str, prompt_file: str) -> str:
         file_path = str(Path(current_file).resolve().parent) + "/prompts/" + prompt_file
         try:
-            f = open(file_path, "r")
-            file_content = f.read()
+            with open(file_path, "r") as f:
+                file_content = f.read()
         except FileNotFoundError as e:
             print(e.__str__())
             raise e
@@ -17,8 +17,8 @@ class PromptReader:
     def read_agent_prompt(current_file: str, prompt_file: str) -> str:
         file_path = str(Path(current_file).resolve().parent) + "/prompts/" + prompt_file
         try:
-            f = open(file_path, "r")
-            file_content = f.read()
+            with open(file_path, "r") as f:
+                file_content = f.read()
         except FileNotFoundError as e:
             print(e.__str__())
             raise e
