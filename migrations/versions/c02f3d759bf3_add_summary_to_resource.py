87853bf57c3ba2e2dc7da95950d240ebe3c13ac5
rakesh-krishna-a-s <akrishna@contlo.com>
2023-06-30 10:54:41 +0530

    migration fix

--- migrations/versions/c02f3d759bf3_add_summary_to_resource.py
+++ migrations/versions/c02f3d759bf3_add_summary_to_resource.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'c02f3d759bf3'
-down_revision = '1d54db311055'
+down_revision = '7a3e336c0fba'
 branch_labels = None
 depends_on = None
 
