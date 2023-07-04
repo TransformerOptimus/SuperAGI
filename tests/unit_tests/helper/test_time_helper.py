c822c8dcca7a9b5421dd0c11cb3a695164519837
anisha-contlo <anisha@contlo.com>
2023-07-05 00:26:38 +0530

    made minor changes

--- tests/unit_tests/helper/test_time_helper.py
+++ tests/unit_tests/helper/test_time_helper.py
@@ -1,7 +1,18 @@
 from superagi.helper.time_helper import get_time_difference, parse_interval_to_seconds
 import pytest
-from datetime import datetime, timedelta
-from unittest.mock import MagicMock
+
+def test_get_time_difference():
+    timestamp1 = "2023-06-26 17:31:08.884322"
+    timestamp2 = "2023-06-27 03:57:42.038497"
+    expected_result = {
+        "years": 0,
+        "months": 0,
+        "days": 0,
+        "hours": 10,
+        "minutes": 26
+    }
+    assert get_time_difference(timestamp1, timestamp2) == expected_result
+
 
 
 def test_parse_interval_to_seconds():
