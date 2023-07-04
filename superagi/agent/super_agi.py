f08fd2d9bbf8aca4e291e68de948be873fa71f3a
Adrian Mohnacs <amohnacs@gmail.com>
2023-07-04 15:35:57 +0700

    Conditional statement expanded for errored API response
    Solved endless loop and hanging UI by extending our error catching by expanding conditional check to properties.

--- superagi/agent/super_agi.py
+++ superagi/agent/super_agi.py
@@ -165,7 +165,7 @@ class SuperAgi:
         total_tokens = current_tokens + TokenCounter.count_message_tokens(response, self.llm.get_model())
         self.update_agent_execution_tokens(current_calls, total_tokens)
 
-        if response['content'] is None:
+        if 'content' not in response or response['content'] is None:
             raise RuntimeError(f"Failed to get response from llm")
         assistant_reply = response['content']
 
