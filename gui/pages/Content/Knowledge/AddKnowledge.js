44c2adcb7e70692a9679fc5eab8bb57042c3f35d
NishantBorthakur <nist.16br@gmail.com>
2023-07-05 17:51:24 +0530

    knowledge first cut

---
+++ gui/pages/Content/Knowledge/AddKnowledge.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles1 from '../Agents/Agents.module.css'
+import {removeTab, setLocalStorageValue} from "@/utils/utils";
+import {ToastContainer, toast} from "react-toastify";
+
+export default function AddKnowledge() {
+
+}
