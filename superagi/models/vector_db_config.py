616acf07c15c3b8622c5abd7939d2b220da3904c
ResoluteStoic <105219157+ResoluteStoic@users.noreply.github.com>
2023-07-06 20:27:21 +0530

    Create vector_db_config.py

---
+++ superagi/models/vector_db_config.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer, Text, String
+
+from superagi.models.base_model import DBBaseModel
+
+
+class Vectordb_config(DBBaseModel):
+    """
+    Vector db related configurations like api_key, environment, and url are stored here
+
+    Attributes:
+        id (int): The unique identifier of the vector db configuration.
+        vector_db_id (int): The identifier of the associated vector db.
+        key (str): The key of the configuration setting.
+        value (str): The value of the configuration setting.
+    """
+
+    __tablename__ = 'vector_db_config'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    vector_db_id = Column(Integer)
+    key = Column(String)
+    value = Column(Text)
+
+    def __repr__(self):
+        """
+        Returns a string representation of the Agent Configuration object.
+
+        Returns:
+            str: String representation of the Agent Configuration.
+
+        """
+        return f"VectorConfiguration(id={self.id}, key={self.key}, value={self.value})"
