52926719cbcebae6c121d83b561bb2d00256f0f6
Maverick-F35 <maverickcode.f35@gmail.com>
2023-07-06 16:02:45 +0530

    Replaced unit tests with pytests

--- tests/unit_tests/tools/duck_duck_go/test_duckduckgo_results.py
+++ tests/unit_tests/tools/duck_duck_go/test_duckduckgo_results.py
@@ -1,28 +1,28 @@
-import unittest
+import pytest
 from superagi.tools.duck_duck_go.duck_duck_go_search import DuckDuckGoSearchTool
 
-class DuckDuckGoSearchToolTest(unittest.TestCase):
-    def setUp(self):
-        self.your_obj = DuckDuckGoSearchTool()  # Create an instance of YourClass
+class TestDuckDuckGoSearchTool:
+    def setup_method(self):
+        self.your_obj = DuckDuckGoSearchTool()  # Create an instance of DuckDuckGoSearchTool
 
     def test_get_raw_duckduckgo_results_empty_query(self):
         query = ""
         expected_result = "[]"
         result = self.your_obj.get_raw_duckduckgo_results(query)
-        self.assertEqual(result, expected_result)
+        assert result == expected_result
 
     def test_get_raw_duckduckgo_results_valid_query(self):
         query = "python"
         expected_result_length = 10
         result = self.your_obj.get_raw_duckduckgo_results(query)
-        self.assertEqual(len(result), expected_result_length)
+        assert len(result) == expected_result_length
 
     def test_get_formatted_webpages(self):
         search_results = [
             {"title": "Result 1", "href": "https://example.com/1"},
             {"title": "Result 2", "href": "https://example.com/2"},
             {"title": "Result 3", "href": "https://example.com/3"},
-            ]
+        ]
         webpages = ["Webpage 1", "Webpage 2", "Webpage 3"]
 
         expected_results = [
@@ -32,30 +32,23 @@ class DuckDuckGoSearchToolTest(unittest.TestCase):
         ]
 
         results = self.your_obj.get_formatted_webpages(search_results, webpages)
-        self.assertEqual(results, expected_results)
+        assert results == expected_results
 
-    
     def test_get_content_from_url_with_empty_links(self):
         links = []
         expected_webpages = []
 
         webpages = self.your_obj.get_content_from_url(links)
-        self.assertEqual(webpages, expected_webpages)
+        assert webpages == expected_webpages
 
     def test_get_formatted_webpages_with_empty_webpages(self):
         search_results = [
             {"title": "Result 1", "href": "https://example.com/1"},
             {"title": "Result 2", "href": "https://example.com/2"},
             {"title": "Result 3", "href": "https://example.com/3"},
-            ]
+        ]
         webpages = []
         expected_results = []
 
         results = self.your_obj.get_formatted_webpages(search_results, webpages)
-        self.assertEqual(results, expected_results)
-
-      
-
-if __name__ == '__main__':
-    unittest.main()
-
+        assert results == expected_results
--- tests/unit_tests/tools/duck_duck_go/test_duckduckgo_toolkit.py
+++ tests/unit_tests/tools/duck_duck_go/test_duckduckgo_toolkit.py
@@ -1,9 +1,9 @@
-import unittest
+import pytest
 from superagi.tools.duck_duck_go.duck_duck_go_search_toolkit import DuckDuckGoToolkit
 from superagi.tools.duck_duck_go.duck_duck_go_search import DuckDuckGoSearchTool
 
-class DuckDuckGoSearchToolKitTest(unittest.TestCase):
-    def setUp(self):
+class TestDuckDuckGoSearchToolKit:
+    def setup_method(self):
         """
         Set up the test fixture.
 
@@ -16,21 +16,20 @@ class DuckDuckGoSearchToolKitTest(unittest.TestCase):
 
     def test_get_tools(self):
         """
-        Test the `get_tools` method of the `SearxSearchToolkit` class.
+        Test the `get_tools` method of the `DuckDuckGoToolkit` class.
 
-        It should return a list of tools, containing one instance of `SearxSearchTool`.
+        It should return a list of tools, containing one instance of `DuckDuckGoSearchTool`.
 
         Returns:
             None
         """
-
         tools = self.toolkit.get_tools()
-        self.assertEqual(1, len(tools))
-        self.assertIsInstance(tools[0], DuckDuckGoSearchTool)
+        assert len(tools) == 1
+        assert isinstance(tools[0], DuckDuckGoSearchTool)
 
     def test_get_env_keys(self):
         """
-        Test the `get_env_keys` method of the `SearxSearchToolkit` class.
+        Test the `get_env_keys` method of the `DuckDuckGoToolkit` class.
 
         It should return an empty list of environment keys.
 
@@ -38,8 +37,4 @@ class DuckDuckGoSearchToolKitTest(unittest.TestCase):
             None
         """
         env_keys = self.toolkit.get_env_keys()
-        self.assertEqual(0, len(env_keys))
-
-if __name__ == '__main__':
-    unittest.main()
-
+        assert len(env_keys) == 0
