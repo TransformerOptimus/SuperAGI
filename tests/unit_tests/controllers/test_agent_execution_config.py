2cf0609560bbc391b708eec3ca08c781d77db851
abhijeet <abhijeet@contlo.com>
2023-07-07 11:10:15 +0530

    Updated test

--- tests/unit_tests/controllers/test_agent_execution_config.py
+++ tests/unit_tests/controllers/test_agent_execution_config.py
@@ -30,7 +30,8 @@ def test_get_agent_execution_configuration_success(mocks):
 def test_get_agent_execution_configuration_not_found():
     with patch('superagi.controllers.agent_execution_config.db') as mock_db:
         mock_db.session.query.return_value.filter.return_value.all.return_value = []
+        mock_db.session.query.return_value.filter.return_value.first.return_value = None
         response = client.get("/agent_executions_configs/details/1")
 
         assert response.status_code == 404
-        assert response.json() == {"detail": "Agent Execution Configuration not found"}
+        assert response.json() == {"detail": "Agent Configuration not found"}
