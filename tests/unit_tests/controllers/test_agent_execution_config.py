1ddd46eaefe0c1ff32e4a514f93f0870d61026da
abhijeet <abhijeet@contlo.com>
2023-07-07 11:45:19 +0530

    Added unit test

--- tests/unit_tests/controllers/test_agent_execution_config.py
+++ tests/unit_tests/controllers/test_agent_execution_config.py
@@ -27,9 +27,20 @@ def test_get_agent_execution_configuration_success(mocks):
         assert response.json() == {"test_key": ['test']}
 
 
-def test_get_agent_execution_configuration_not_found():
+def test_get_agent_execution_configuration_not_found_failure():
     with patch('superagi.controllers.agent_execution_config.db') as mock_db:
         mock_db.session.query.return_value.filter.return_value.all.return_value = []
+        mock_db.session.query.return_value.filter.return_value.first.return_value = None
+        response = client.get("/agent_executions_configs/details/1")
+
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Agent Configuration not found"}
+
+
+def test_get_agent_execution_configuration_not_found_success():
+    with patch('superagi.controllers.agent_execution_config.db') as mock_db:
+        mock_execution_config = mocks
+        mock_db.session.query.return_value.filter.return_value.all.side_effect = [[], mock_execution_config]
         mock_db.session.query.return_value.filter.return_value.first.return_value = None
         response = client.get("/agent_executions_configs/details/1")
 
