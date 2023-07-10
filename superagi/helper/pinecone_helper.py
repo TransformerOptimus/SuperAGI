88dd1b9911ee488acdc60c1164237fe732dd9911
Tarraann <jot.taran15522@gmail.com>
2023-07-10 18:48:45 +0530

    Completed helpers

--- superagi/helper/pinecone_helper.py
+++ superagi/helper/pinecone_helper.py
@@ -1,18 +1,48 @@
 from sqlalchemy.orm import Session
+from superagi.models.vector_db_config import VectordbConfig
+import pinecone
 
 class PineconeHelper:
     
     def __init__(self, session):
         self.session = session
-    
-    def get_dimensions(self, vector_index):
-        return {
-            "success": True,
-            "dimensions": "1536"
-        }
-    
-    def get_pinecone_index_state(self, vector_index):
-        return {
-            "success": True,
-            "state": "CUSTOM"
-        }
+        
+    def get_pinecone_client(self, vector_db):
+        api_key = self.session.query(VectordbConfig).filter(VectordbConfig.vector_db_id == vector_db.id, VectordbConfig.key == "API_KEY").first()
+        environment = self.session.query(VectordbConfig).filter(VectordbConfig.vector_db_id == vector_db.id, VectordbConfig.key == "ENVIRONMENT").first()
+        pinecone.init(api_key=api_key.value, environment=environment.value)
+
+    def get_dimensions(self,vector_db, vector_index):
+        try:
+            self.get_pinecone_client(vector_db)
+            dimensions = pinecone.Index(vector_index.name).describe_index_stats().dimension
+            data = {
+                "success": True,
+                "dimensions": str(dimensions)
+            }
+        except:
+            data = {
+                "success": False,
+                "message": "Failed to Connect"
+            }
+        return data
+
+    def get_pinecone_index_state(self, vector_db, vector_index):
+        try:
+            self.get_pinecone_client(vector_db)
+            total_vector_count = pinecone.Index(vector_index.name).describe_index_stats().total_vector_count
+            if total_vector_count > 0:
+                state = "CUSTOM"
+            else:
+                state = "NONE"
+            data = {
+                "success": True,
+                "state": state
+            }
+        except:
+            data = {
+                "success": False,
+                "message": "Failed to Connect"
+            }
+
+        return data
--- superagi/helper/qdrant_helper.py
+++ superagi/helper/qdrant_helper.py
@@ -1,18 +1,53 @@
 from sqlalchemy.orm import Session
+from superagi.models.vector_db_config import VectordbConfig
+from qdrant_client import models, QdrantClient
 
 class QdrantHelper:
     
-    def __init__(self, session):
+    def __init__(self, session, vector_db):
         self.session = session
+
+    def get_qdrant_client(self, vector_db):
+        api_key = self.session.query(VectordbConfig).filter(VectordbConfig.vector_db_id == vector_db.id, VectordbConfig.key == "API_KEY").first()
+        url = self.session.query(VectordbConfig).filter(VectordbConfig.vector_db_id == vector_db.id, VectordbConfig.key == "URL").first()
+        qdrant_client = QdrantClient(
+            api_key=api_key.value,
+            url=url.value
+        )
+        return qdrant_client
     
-    def get_dimensions(self, vector_index):
-        return {
-            "success": True,
-            "dimensions": "1536"
-        }
+    def get_dimensions(self, vector_db, vector_index):
+        try:
+            qdrant_client = self.get_qdrant_client(vector_db)
+            collection_info = qdrant_client.get_collection(collection_name=vector_index.name)
+            dimensions = collection_info.config.params.vectors.size
+            data = {
+                "success": True,
+                "dimensions": str(dimensions)
+            }
+        except:
+            data = {
+                "success": False,
+                "message": "Failed to Connect"
+            }
+        return data
     
-    def get_qdrant_index_state(self, vector_index):
-        return {
-            "success": True,
-            "state": "CUSTOM"
-        }
+    def get_qdrant_index_state(self, vector_db, vector_index):
+        try:
+            qdrant_client = self.get_qdrant_client(vector_db)
+            collection_info = qdrant_client.get_collection(collection_name=vector_index.name)
+            total_vector_count = collection_info.vectors_count
+            if total_vector_count > 0:
+                state = "CUSTOM"
+            else:
+                state = "NONE"
+            data = {
+                "success": True,
+                "state": state
+            }
+        except:
+            data = {
+                "success": False,
+                "message": "Failed to Connect"
+            }
+        return data
