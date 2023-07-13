5c83d57e6f01d286769812d9e03aa878fc7df39a
abhijeet <abhijeet@contlo.com>
2023-07-13 17:42:56 +0530

    minor fix

--- superagi/types/storage_types.py
+++ superagi/types/storage_types.py
@@ -7,9 +7,9 @@ class StorageType(Enum):
 
     @classmethod
     def get_storage_type(cls, store):
-        store = store.upper()
         if store is None:
             raise ValueError("Storage type cannot be None.")
+        store = store.upper()
         if store in cls.__members__:
             return cls[store]
         raise ValueError(f"{store} is not a valid storage name.")
