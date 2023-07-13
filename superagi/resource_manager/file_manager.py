a5944df026088ab14385fd685315bbb26a50b480
COLONAYUSH <ayushkaps9462@gmail.com>
2023-07-13 11:58:36 +0530

    Merge resolve - Addition of overwrite logic

--- superagi/resource_manager/file_manager.py
+++ superagi/resource_manager/file_manager.py
@@ -1,27 +1,25 @@
 import csv
-import os
-
-from llama_index import SimpleDirectoryReader
-from llama_index.indices.response import ResponseMode
-from llama_index.schema import Document
 from sqlalchemy.orm import Session
-
-from superagi.config.config import get_config
+import os
 from superagi.helper.resource_helper import ResourceHelper
 from superagi.helper.s3_helper import S3Helper
 from superagi.lib.logger import logger
+from superagi.models.agent import Agent
+from superagi.models.agent_execution import AgentExecution
 from superagi.types.storage_types import StorageType
-from superagi.types.vector_store_types import VectorStoreType
-
-
 class FileManager:
-    def __init__(self, session: Session, agent_id: int = None):
+    def __init__(self, session: Session, agent_id: int = None, agent_execution_id: int = None):
         self.session = session
         self.agent_id = agent_id
-
+        self.agent_execution_id = agent_execution_id
     def write_binary_file(self, file_name: str, data):
         if self.agent_id is not None:
-            final_path = ResourceHelper.get_agent_resource_path(file_name, self.agent_id)
+            final_path = ResourceHelper.get_agent_write_resource_path(file_name,
+                                                                      Agent.get_agent_from_id(self.session,
+                                                                                              self.agent_id),
+                                                                      AgentExecution.get_agent_execution_from_id(
+                                                                          self.session,
+                                                                          self.agent_execution_id))
         else:
             final_path = ResourceHelper.get_resource_path(file_name)
         try:
@@ -33,11 +31,14 @@ class FileManager:
             return f"Binary {file_name} saved successfully"
         except Exception as err:
             return f"Error write_binary_file: {err}"
-
     def write_to_s3(self, file_name, final_path):
         with open(final_path, 'rb') as img:
             resource = ResourceHelper.make_written_file_resource(file_name=file_name,
-                                                                 agent_id=self.agent_id, channel="OUTPUT")
+                                                                 agent=Agent.get_agent_from_id(self.session,
+                                                                                               self.agent_id),
+                                                                 agent_execution=AgentExecution
+                                                                 .get_agent_execution_from_id(self.session,
+                                                                                              self.agent_execution_id))
             if resource is not None:
                 self.session.add(resource)
                 self.session.commit()
@@ -45,10 +46,14 @@ class FileManager:
                 if resource.storage_type == StorageType.S3.value:
                     s3_helper = S3Helper()
                     s3_helper.upload_file(img, path=resource.path)
-
     def write_file(self, file_name: str, content):
         if self.agent_id is not None:
-            final_path = ResourceHelper.get_agent_resource_path(file_name, self.agent_id)
+            final_path = ResourceHelper.get_agent_write_resource_path(file_name,
+                                                                      agent=Agent.get_agent_from_id(self.session,
+                                                                                                    self.agent_id),
+                                                                      agent_execution=AgentExecution
+                                                                      .get_agent_execution_from_id(self.session,
+                                                                                                   self.agent_execution_id))
         else:
             final_path = ResourceHelper.get_resource_path(file_name)
         try:
@@ -60,10 +65,14 @@ class FileManager:
             return f"{file_name} - File written successfully"
         except Exception as err:
             return f"Error write_file: {err}"
-
     def write_csv_file(self, file_name: str, csv_data):
         if self.agent_id is not None:
-            final_path = ResourceHelper.get_agent_resource_path(file_name, self.agent_id)
+            final_path = ResourceHelper.get_agent_write_resource_path(file_name,
+                                                                      agent=Agent.get_agent_from_id(self.session,
+                                                                                                    self.agent_id),
+                                                                      agent_execution=AgentExecution
+                                                                      .get_agent_execution_from_id(self.session,
+                                                                                                   self.agent_execution_id))
         else:
             final_path = ResourceHelper.get_resource_path(file_name)
         try:
@@ -76,10 +85,12 @@ class FileManager:
             return f"{file_name} - File written successfully"
         except Exception as err:
             return f"Error write_csv_file: {err}"
-
     def get_agent_resource_path(self, file_name: str):
-        return ResourceHelper.get_agent_resource_path(file_name, self.agent_id)
-
+         return ResourceHelper.get_agent_write_resource_path(file_name, agent=Agent.get_agent_from_id(self.session,
+                                                                                                     self.agent_id),
+                                                            agent_execution=AgentExecution
+                                                            .get_agent_execution_from_id(self.session,
+                                                                                         self.agent_execution_id))
     def overwrite_file(self, file_name: str, content):
         if self.agent_id is not None:
             final_path = ResourceHelper.get_agent_resource_path(file_name, self.agent_id)
@@ -94,7 +105,6 @@ class FileManager:
             return f"{file_name} - File overwritten successfully"
         except Exception as err:
             return f"Error: {err}"
-
     def read_file(self, file_name: str):
         if self.agent_id is not None:
             final_path = ResourceHelper.get_agent_resource_path(file_name, self.agent_id)
@@ -107,11 +117,9 @@ class FileManager:
             return content
         except Exception as err:
             return f"Error while reading file {file_name}: {err}"
-
     def get_files(self):
             """
             Gets all file names generated by the CodingTool.
-
             Returns:
                 A list of file names.
             """
@@ -119,13 +127,10 @@ class FileManager:
                 final_path = ResourceHelper.get_agent_resource_path("", self.agent_id)
             else:
                 final_path = ResourceHelper.get_resource_path("")
-
             try:
                 # List all files in the directory
                 files = os.listdir(final_path)
-
             except Exception as err:
                 logger.error(f"Error while accessing files in {final_path}: {err}")
                 files = []
-
             return files
