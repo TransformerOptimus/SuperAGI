c2e4d296348a7ff2964e8246e4f370767be019ca
luciferlinx <129729795+luciferlinx101@users.noreply.github.com>
2023-07-14 19:37:05 +0530

    Revert "ui changes (#768)"
    This reverts commit ae83a16642e7ea74e16052622ac2097095ade85d.

--- superagi/jobs/scheduling_executor.py
+++ superagi/jobs/scheduling_executor.py
@@ -10,7 +10,6 @@ from superagi.models.agent import Agent
 from superagi.models.agent_config import AgentConfiguration
 from superagi.models.agent_execution import AgentExecution
 from superagi.models.agent_execution_config import AgentExecutionConfiguration
-from superagi.apm.event_handler import EventHandler
 
 from superagi.models.db import connect_db
 
@@ -34,36 +33,28 @@ class ScheduledAgentExecutor:
         if not agent:
             raise HTTPException(status_code=404, detail="Agent not found")
 
-
-
         start_step_id = AgentWorkflow.fetch_trigger_step_id(session, agent.agent_workflow_id)
         db_agent_execution = AgentExecution(status="RUNNING", last_execution_time=datetime.now(),
                                             agent_id=agent_id, name=name, num_of_calls=0,
                                             num_of_tokens=0,
                                             current_step_id=start_step_id)
-
+        
         session.add(db_agent_execution)
         session.commit()
-
+        
         goal_value = session.query(AgentConfiguration.value).filter(AgentConfiguration.agent_id == agent_id).filter(AgentConfiguration.key == 'goal').first()[0]
         instruction_value = session.query(AgentConfiguration.value).filter(AgentConfiguration.agent_id == agent_id).filter(AgentConfiguration.key == 'instruction').first()[0]
-
+                
         agent_execution_configs = {
             "goal": goal_value,
             "instruction": instruction_value
         }
 
-
-        AgentExecutionConfiguration.add_or_update_agent_execution_config(session= session, execution=db_agent_execution,agent_execution_configs=agent_execution_configs)
-
-
-        organisation = agent.get_agent_organisation(session)
-        model = session.query(AgentConfiguration.value).filter(AgentConfiguration.agent_id == agent_id).filter(AgentConfiguration.key == 'model').first()[0]
-        EventHandler(session=session).create_event('run_created', {'agent_execution_id': db_agent_execution.id,'agent_execution_name':db_agent_execution.name}, agent_id, organisation.id if organisation else 0),
-        EventHandler(session=session).create_event('agent_created', {'agent_name': agent.name, 'model': model}, agent_id, organisation.id if organisation else 0)
-
-        session.commit()
-
+        
+        AgentExecutionConfiguration.add_or_update_agent_execution_config(session= session, execution=db_agent_execution,
+                                                                        agent_execution_configs=agent_execution_configs)
+        
+        
         if db_agent_execution.status == "RUNNING":
             execute_agent.delay(db_agent_execution.id, datetime.now())
 
