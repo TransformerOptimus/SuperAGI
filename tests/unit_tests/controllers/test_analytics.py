ca9f8a7127b9f524b4284a858a27004d3477091a
jedan2506 <parth.pathak@science.christuniversity.in>
2023-07-14 21:38:21 +0530

    ui changes

--- tests/unit_tests/controllers/test_analytics.py
+++ tests/unit_tests/controllers/test_analytics.py
@@ -1,59 +1,63 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
+from fastapi_jwt_auth import AuthJWT
+from superagi.helper.auth import get_user_organisation
 import pytest
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
-# add a fixture for organisation
-@pytest.fixture
-def mock_organisation():
-    mock_org = MagicMock()
-    mock_org.id = 1
-    return mock_org
+def test_get_metrics_success():
+    with patch('superagi.helper.auth.get_user_organisation') as mock_org, \
+        patch('superagi.apm.analytics_helper.AnalyticsHelper') as mock_helper:
 
-# updated the test functions to use mock_organisation and mock session object
-@patch('superagi.apm.analytics_helper.db.session')
-@patch('superagi.helper.auth.get_user_organisation', return_value=mock_organisation)
-def test_get_metrics_success(get_user_organisation, mock_session):
-    with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
+        mock_org.return_value = 'dummy_organisation'
         mock_helper().calculate_run_completed_metrics.return_value = {'total_tokens': 10, 'total_calls': 5, 'runs_completed': 2}
+
         response = client.get("analytics/metrics")
         assert response.status_code == 200
         assert response.json() == {'total_tokens': 10, 'total_calls': 5, 'runs_completed': 2}
 
-@patch('superagi.apm.analytics_helper.db.session')
-@patch('superagi.helper.auth.get_user_organisation', return_value=mock_organisation)
-def test_get_agents_success(get_user_organisation, mock_session):
-    with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
+def test_get_agents_success():
+    with patch('superagi.helper.auth.get_user_organisation') as mock_org, \
+        patch('superagi.apm.analytics_helper.AnalyticsHelper') as mock_helper:
+
+        mock_org.return_value = 'dummy_organisation'
         mock_helper().fetch_agent_data.return_value = {"agent_details": "mock_details", "model_info": "mock_info"}
+
         response = client.get("analytics/agents/all")
         assert response.status_code == 200
         assert response.json() == {"agent_details": "mock_details", "model_info": "mock_info"}
 
-@patch('superagi.apm.analytics_helper.db.session')
-@patch('superagi.helper.auth.get_user_organisation', return_value=mock_organisation)
-def test_get_agent_runs_success(get_user_organisation, mock_session):
-    with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
+def test_get_agent_runs_success():
+    with patch('superagi.helper.auth.get_user_organisation') as mock_org, \
+        patch('superagi.apm.analytics_helper.AnalyticsHelper') as mock_helper:
+
+        mock_org.return_value = 'dummy_organisation'
         mock_helper().fetch_agent_runs.return_value = "mock_agent_runs"
+
         response = client.get("analytics/agents/1")
         assert response.status_code == 200
         assert response.json() == "mock_agent_runs"
 
-@patch('superagi.apm.analytics_helper.db.session')
-@patch('superagi.helper.auth.get_user_organisation', return_value=mock_organisation)
-def test_get_active_runs_success(get_user_organisation, mock_session):
-    with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
+def test_get_active_runs_success():
+    with patch('superagi.helper.auth.get_user_organisation') as mock_org, \
+        patch('superagi.apm.analytics_helper.AnalyticsHelper') as mock_helper:
+
+        mock_org.return_value = 'dummy_organisation'
         mock_helper().get_active_runs.return_value = ["mock_run_1", "mock_run_2"]
+
         response = client.get("analytics/runs/active")
         assert response.status_code == 200
         assert response.json() == ["mock_run_1", "mock_run_2"]
 
-@patch('superagi.apm.analytics_helper.db.session')
-@patch('superagi.helper.auth.get_user_organisation', return_value=mock_organisation)
-def test_get_tools_user_success(get_user_organisation, mock_session):
-    with patch('superagi.controllers.analytics.ToolsHandler') as mock_handler:
+def test_get_tools_user_success():
+    with patch('superagi.helper.auth.get_user_organisation') as mock_org, \
+        patch('superagi.apm.tools_handler.ToolsHandler') as mock_handler:
+
+        mock_org.return_value = 'dummy_organisation'
         mock_handler().calculate_tool_usage.return_value = ["tool1", "tool2"]
+
         response = client.get("analytics/tools/used")
         assert response.status_code == 200
         assert response.json() == ["tool1", "tool2"]
