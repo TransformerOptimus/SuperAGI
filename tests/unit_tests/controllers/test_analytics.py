75ee240b540f9986afd3ec0bc2b3c53d2de9c740
jedan2506 <parth.pathak@science.christuniversity.in>
2023-07-16 13:37:11 +0530

    ui changes

--- tests/unit_tests/controllers/test_analytics.py
+++ tests/unit_tests/controllers/test_analytics.py
@@ -9,6 +9,12 @@ class MockSession:
     def __init__(self):
         pass
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
     def query(self, *args, **kwargs):
         pass
 
