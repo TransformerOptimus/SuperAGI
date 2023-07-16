5b0213dd9ef11fd34e4ea6fd51527bf0c1595b44
jedan2506 <parth.pathak@science.christuniversity.in>
2023-07-17 00:16:29 +0530

    ui changes

--- tests/unit_tests/controllers/test_analytics.py
+++ tests/unit_tests/controllers/test_analytics.py
@@ -1,50 +1,41 @@
+from unittest.mock import patch
+import pytest
 from fastapi.testclient import TestClient
-from unittest.mock import MagicMock, patch
-from main import app  # assuming your FastAPI app is defined here
+from main import app
 
 client = TestClient(app)
 
-@patch("superagi.helper.auth.get_user_organisation")
-@patch("superagi.apm.analytics_helper.AnalyticsHelper.calculate_run_completed_metrics")
-def test_get_metrics(mock_calculate_metrics, mock_get_org):
-    mock_get_org.return_value.id = 1
-    mock_calculate_metrics.return_value = {"metrics": "data"}
-    response = client.get("/metrics")
-    assert response.status_code == 200
-    assert response.json() == {"metrics": "data"}
+def test_get_metrics_success():
+    with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
+        mock_helper().calculate_run_completed_metrics.return_value = {'total_tokens': 10, 'total_calls': 5, 'runs_completed': 2}
+        response = client.get("analytics/metrics")
+        assert response.status_code == 200
+        assert response.json() == {'total_tokens': 10, 'total_calls': 5, 'runs_completed': 2}
 
-@patch("superagi.helper.auth.get_user_organisation")
-@patch("superagi.apm.analytics_helper.AnalyticsHelper.fetch_agent_data")
-def test_get_agents(mock_fetch_agent_data, mock_get_org):
-    mock_get_org.return_value.id = 1
-    mock_fetch_agent_data.return_value = {"agents": "data"}
-    response = client.get("/agents/all")
-    assert response.status_code == 200
-    assert response.json() == {"agents": "data"}
+def test_get_agents_success():
+    with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
+        mock_helper().fetch_agent_data.return_value = {"agent_details": "mock_details", "model_info": "mock_info"}
+        response = client.get("analytics/agents/all")
+        assert response.status_code == 200
+        assert response.json() == {"agent_details": "mock_details", "model_info": "mock_info"}
 
-@patch("superagi.helper.auth.get_user_organisation")
-@patch("superagi.apm.analytics_helper.AnalyticsHelper.fetch_agent_runs")
-def test_get_agent_runs(mock_fetch_agent_runs, mock_get_org):
-    mock_get_org.return_value.id = 1
-    mock_fetch_agent_runs.return_value = {"agent_runs": "data"}
-    response = client.get("/agents/1")  # assuming an agent with id 1 exists
-    assert response.status_code == 200
-    assert response.json() == {"agent_runs": "data"}
+def test_get_agent_runs_success():
+    with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
+        mock_helper().fetch_agent_runs.return_value = "mock_agent_runs"
+        response = client.get("analytics/agents/1")
+        assert response.status_code == 200
+        assert response.json() == "mock_agent_runs"
 
-@patch("superagi.helper.auth.get_user_organisation")
-@patch("superagi.apm.analytics_helper.AnalyticsHelper.get_active_runs")
-def test_get_active_runs(mock_get_active_runs, mock_get_org):
-    mock_get_org.return_value.id = 1
-    mock_get_active_runs.return_value = {"active_runs": "data"}
-    response = client.get("/runs/active")
-    assert response.status_code == 200
-    assert response.json() == {"active_runs": "data"}
+def test_get_active_runs_success():
+    with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
+        mock_helper().get_active_runs.return_value = ["mock_run_1", "mock_run_2"]
+        response = client.get("analytics/runs/active")
+        assert response.status_code == 200
+        assert response.json() == ["mock_run_1", "mock_run_2"]
 
-@patch("superagi.helper.auth.get_user_organisation")
-@patch("superagi.apm.tools_handler.ToolsHandler.calculate_tool_usage")
-def test_get_tools_used(mock_get_tools_used, mock_get_org):
-    mock_get_org.return_value.id = 1
-    mock_get_tools_used.return_value = {"tools_used": "data"}
-    response = client.get("/tools/used")
-    assert response.status_code == 200
-    assert response.json() == {"tools_used": "data"}
+def test_get_tools_user_success():
+    with patch('superagi.controllers.analytics.ToolsHandler') as mock_handler:
+        mock_handler().calculate_tool_usage.return_value = ["tool1", "tool2"]
+        response = client.get("analytics/tools/used")
+        assert response.status_code == 200
+        assert response.json() == ["tool1", "tool2"]
