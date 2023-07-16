46144c93ca49f4153a54f5f11cfb801c63fb419a
jedan2506 <parth.pathak@science.christuniversity.in>
2023-07-17 00:03:53 +0530

    ui changes

--- tests/unit_tests/controllers/test_analytics.py
+++ tests/unit_tests/controllers/test_analytics.py
@@ -1,85 +1,31 @@
-from fastapi.testclient import TestClient
-from unittest.mock import patch
 import pytest
-
-from superagi.apm.analytics import router as analytics_router
-from superagi.models.organisation import Organisation
-from superagi.apm.analytics_helper import AnalyticsHelper
-from fastapi import FastAPI
-
-app = FastAPI()
-app.include_router(analytics_router)
+import requests
+from fastapi.testclient import TestClient
+from main import app  # assuming your FastAPI app is defined here
 
 client = TestClient(app)
 
-organisation = Organisation(id=1)
-metrics = {"total_tokens": 1000, "total_calls": 500, "runs_completed": 80}
-agents = [{"id": 1, "name": "agent1"}, {"id": 2, "name": "agent2"}]
-runs = [{"id": 1, "run_name": "run1"}, {"id": 2, "run_name": "run2"}]
-active_runs = [{"id": 1, "run_name": "run1", "status": "active"}, {"id": 2, "run_name": "run2", "status": "active"}]
-tools_used = [{"tool_id": 1, "tool_name": "tool1"}, {"tool_id": 2, "tool_name": "tool2"}]
-
-class MockAnalyticsHelper:
-    def __init__(self, session, organisation_id):
-        pass
-
-    def calculate_run_completed_metrics(self):
-        return metrics
-
-    def fetch_agent_data(self):
-        return agents
-
-    def fetch_agent_runs(self, agent_id):
-        return runs
-
-    def get_active_runs(self):
-        return active_runs
-
-
-class MockToolsHandler:
-    def __init__(self, session, organisation_id):
-        pass
-
-    def calculate_tool_usage(self):
-        return tools_used
-
-
-@pytest.fixture
-def mock_get_user_organisation():
-    return organisation
-
-@patch("superagi.apm.analytics.AnalyticsHelper", new=MockAnalyticsHelper)
-@patch("superagi.apm.analytics.ToolsHandler", new=MockToolsHandler)
-@patch("superagi.apm.analytics.get_user_organisation", mock_get_user_organisation)
-def test_route_metrics():
+def test_get_metrics():
     response = client.get("/metrics")
     assert response.status_code == 200
-    assert response.json() == metrics
+    assert "metrics" in response.json()
 
-@patch("superagi.apm.analytics.AnalyticsHelper", new=MockAnalyticsHelper)
-@patch("superagi.apm.analytics.get_user_organisation", mock_get_user_organisation)
-def test_route_agents():
+def test_get_agents():
     response = client.get("/agents/all")
     assert response.status_code == 200
-    assert response.json() == agents
+    assert "agents" in response.json()
 
-@patch("superagi.apm.analytics.AnalyticsHelper", new=MockAnalyticsHelper)
-@patch("superagi.apm.analytics.get_user_organisation", mock_get_user_organisation)
-def test_route_agents_runs():
-    response = client.get("/agents/1")
+def test_get_agent_runs():
+    response = client.get("/agents/1")  # assuming an agent with id 1 exists
     assert response.status_code == 200
-    assert response.json() == runs
+    assert "runs" in response.json()
 
-@patch("superagi.apm.analytics.AnalyticsHelper", new=MockAnalyticsHelper)
-@patch("superagi.apm.analytics.get_user_organisation", mock_get_user_organisation)
-def test_route_active_runs():
+def test_get_active_runs():
     response = client.get("/runs/active")
     assert response.status_code == 200
-    assert response.json() == active_runs
+    assert "active_runs" in response.json()
 
-@patch("superagi.apm.analytics.ToolsHandler", new=MockToolsHandler)
-@patch("superagi.apm.analytics.get_user_organisation", mock_get_user_organisation)
-def test_route_tools_used():
+def test_get_tools_used():
     response = client.get("/tools/used")
     assert response.status_code == 200
-    assert response.json() == tools_used
+    assert "tools_used" in response.json()
