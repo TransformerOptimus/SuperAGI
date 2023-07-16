724d2faeb4535a17eec3c5cd355d781a43cb38a3
jedan2506 <parth.pathak@science.christuniversity.in>
2023-07-16 13:54:49 +0530

    ui changes

--- tests/unit_tests/controllers/test_analytics.py
+++ tests/unit_tests/controllers/test_analytics.py
@@ -2,38 +2,49 @@ from unittest.mock import patch
 import pytest
 from fastapi.testclient import TestClient
 from main import app
+from sqlalchemy.orm import sessionmaker, Session
 
 client = TestClient(app)
 
-def test_get_metrics_success():
+@patch('sqlalchemy.orm.sessionmaker')
+def test_get_metrics_success(mock_ses_maker: Session):
+    mock_ses_maker.return_value = Session
     with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
         mock_helper().calculate_run_completed_metrics.return_value = {'total_tokens': 10, 'total_calls': 5, 'runs_completed': 2}
         response = client.get("analytics/metrics")
         assert response.status_code == 200
         assert response.json() == {'total_tokens': 10, 'total_calls': 5, 'runs_completed': 2}
 
-def test_get_agents_success():
+@patch('sqlalchemy.orm.sessionmaker')
+def test_get_agents_success(mock_ses_maker: Session):
+    mock_ses_maker.return_value = Session
     with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
         mock_helper().fetch_agent_data.return_value = {"agent_details": "mock_details", "model_info": "mock_info"}
         response = client.get("analytics/agents/all")
         assert response.status_code == 200
         assert response.json() == {"agent_details": "mock_details", "model_info": "mock_info"}
 
-def test_get_agent_runs_success():
+@patch('sqlalchemy.orm.sessionmaker')
+def test_get_agent_runs_success(mock_ses_maker: Session):
+    mock_ses_maker.return_value = Session
     with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
         mock_helper().fetch_agent_runs.return_value = "mock_agent_runs"
         response = client.get("analytics/agents/1")
         assert response.status_code == 200
         assert response.json() == "mock_agent_runs"
 
-def test_get_active_runs_success():
+@patch('sqlalchemy.orm.sessionmaker')
+def test_get_active_runs_success(mock_ses_maker: Session):
+    mock_ses_maker.return_value = Session
     with patch('superagi.controllers.analytics.AnalyticsHelper') as mock_helper:
         mock_helper().get_active_runs.return_value = ["mock_run_1", "mock_run_2"]
         response = client.get("analytics/runs/active")
         assert response.status_code == 200
         assert response.json() == ["mock_run_1", "mock_run_2"]
 
-def test_get_tools_user_success():
+@patch('sqlalchemy.orm.sessionmaker')
+def test_get_tools_user_success(mock_ses_maker: Session):
+    mock_ses_maker.return_value = Session
     with patch('superagi.controllers.analytics.ToolsHandler') as mock_handler:
         mock_handler().calculate_tool_usage.return_value = ["tool1", "tool2"]
         response = client.get("analytics/tools/used")
