2a042f67750e0c4046ce10f192ce360c8b48e601
Tarraann <jot.taran15522@gmail.com>
2023-07-18 18:04:19 +0530

    changes

--- superagi/controllers/knowledges.py
+++ superagi/controllers/knowledges.py
@@ -69,8 +69,6 @@ def get_user_knowledge_list(organisation = Depends(get_user_organisation)):
 def get_knowledge_details(knowledge_name: str):
     knowledge_data = Knowledges.fetch_knowledge_details_marketplace(knowledge_name)
     knowledge_config_data = KnowledgeConfigs.fetch_knowledge_config_details_marketplace(knowledge_data["id"])
-    knowledge_config_data["introduction"] = eval(knowledge_config_data["introduction"])
-    knowledge_config_data["use_cases"] = eval(knowledge_config_data["use_cases"])
     knowledge_data_with_config = knowledge_data | knowledge_config_data
     knowledge_data_with_config["install_number"] = MarketPlaceStats.get_knowledge_installation_number(knowledge_data_with_config["id"])
     knowledge_data_with_config["updated_at"] = datetime.strftime(knowledge_data_with_config["updated_at"], '%d %B %Y')
