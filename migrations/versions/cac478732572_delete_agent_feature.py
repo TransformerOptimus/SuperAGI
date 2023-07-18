1d2ca408c1326c3547ba8168dcd2d4af37b607f9
Boundless Asura <nihir@contlo.com>
2023-07-18 18:11:37 +0530

    fixed default value of is_deleted

--- migrations/versions/cac478732572_delete_agent_feature.py
+++ migrations/versions/cac478732572_delete_agent_feature.py
@@ -8,7 +8,6 @@ Create Date: 2023-07-13 17:18:42.003412
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = 'cac478732572'
 down_revision = 'e39295ec089c'
@@ -17,7 +16,8 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column('agents', sa.Column('is_deleted', sa.Boolean(), nullable=True,default=False))
+    op.add_column('agents', sa.Column('is_deleted', sa.Boolean(), nullable=True, server_default=sa.false()))
+
 
 def downgrade() -> None:
     op.drop_column('agents', 'is_deleted')
