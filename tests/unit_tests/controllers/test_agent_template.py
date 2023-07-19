863b6b738b6f208af2a5b7c5f2e5b158287773c4
anisha1607 <anishagupta1607@gmail.com>
2023-07-19 17:01:16 +0530

    added another unit test

--- tests/unit_tests/controllers/test_agent_template.py
+++ tests/unit_tests/controllers/test_agent_template.py
@@ -15,7 +15,7 @@ def test_edit_agent_template_success(mock_get_user_org, mock_auth_db, mock_db):
     # mock_agent_goals = AgentTemplateConfig()
 
     # Create a mock edited agent configuration
-    mock_edited_agent_configs = {
+    mock_updated_agent_configs = {
         "name": "Updated Agent Template",
         "description": "Updated Description",
         "agent_configs": {
@@ -48,7 +48,7 @@ def test_edit_agent_template_success(mock_get_user_org, mock_auth_db, mock_db):
 
 
     # Call the endpoint
-    response = client.put("agent_templates/update_agent_template/1", json=mock_edited_agent_configs)
+    response = client.put("agent_templates/update_agent_template/1", json=mock_updated_agent_configs)
 
     assert response.status_code == 200
         
@@ -85,3 +85,43 @@ def test_edit_agent_template_failure(mock_get_user_org, mock_auth_db, mock_db):
     # Verify: The database commit method should not have been called because the agent template was not found.
     session_mock.commit.assert_not_called()
     session_mock.flush.assert_not_called()
+
+
+@patch('superagi.controllers.agent_template.db')
+@patch('superagi.helper.auth.db')
+@patch('superagi.helper.auth.get_user_organisation')
+def test_edit_agent_template_with_new_config_success(mock_get_user_org, mock_auth_db, mock_db):
+    # Create a mock agent template
+    mock_agent_template = AgentTemplate(id=1, name="Test Agent Template", description="Test Description")
+
+    # Create a mock edited agent configuration
+    mock_updated_agent_configs = {
+        "name": "Updated Agent Template",
+        "description": "Updated Description",
+        "agent_configs": {
+            "new_config_key": "New config value"  # This is a new config
+        }
+    }    
+
+    # Mocking the user organisation
+    mock_get_user_org.return_value = MagicMock(id=1)
+
+    # Create a session mock
+    session_mock = MagicMock()
+    mock_db.session = session_mock
+    mock_db.session.query.return_value.filter.return_value.first.return_value = mock_agent_template
+    mock_db.session.commit.return_value = None
+    mock_db.session.add.return_value = None
+    mock_db.session.flush.return_value = None
+
+    # Call the endpoint
+    response = client.put("agent_templates/update_agent_template/1", json=mock_updated_agent_configs)
+
+    assert response.status_code == 200
+
+    # Verify changes in the mock agent template
+    assert mock_agent_template.name == "Updated Agent Template"
+    assert mock_agent_template.description == "Updated Description"
+
+    session_mock.commit.assert_called()
+    session_mock.flush.assert_called()
