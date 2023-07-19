16af2537b61bf289ef0b0f73816e377f65da3eca
Taran <97586318+Tarraann@users.noreply.github.com>
2023-07-19 13:46:03 +0530

    Twitter and Google Oauth for Web (#808)
    Co-authored-by: Tarraann <jot.taran15522@gmail.com>

--- gui/pages/Dashboard/Content.js
+++ gui/pages/Dashboard/Content.js
@@ -340,7 +340,7 @@ export default function Content({env, selectedView, selectedProjectId, organisat
                       <AgentWorkspace internalId={tab.internalId || index} agentId={tab.id} agentName={tab.name} selectedView={selectedView}
                                       agents={agents} fetchAgents={fetchAgents}/>}
                     {tab.contentType === 'Toolkits' &&
-                      <ToolkitWorkspace internalId={tab.internalId || index} toolkitDetails={toolkitDetails}/>}
+                      <ToolkitWorkspace env={env} internalId={tab.internalId || index} toolkitDetails={toolkitDetails}/>}
                     {tab.contentType === 'Settings' && <Settings organisationId={organisationId}/>}
                     {tab.contentType === 'Marketplace' && <Market env={env} selectedView={selectedView}/>}
                     {tab.contentType === 'Add_Toolkit' && <AddTool internalId={tab.internalId || index}/>}
