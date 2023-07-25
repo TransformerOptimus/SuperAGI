44175c25257c40b38899c72d27d5c32fa5d735b3
Tarraann <jot.taran15522@gmail.com>
2023-07-25 18:40:22 +0530

    changes

--- tests/unit_tests/controllers/test_knowledge_configs.py
+++ tests/unit_tests/controllers/test_knowledge_configs.py
@@ -9,23 +9,10 @@ client = TestClient(app)
 
 def test_get_marketplace_knowledge_configs_details(mocker):
     knowledge_id = 1
-    mock_knowledge_configs = MagicMock()
-    # Mock the query function
-    mock_db = mocker.patch('superagi.controllers.knowledge_configs.db')
-    mock_db.session.query.return_value.filter.return_value.all.return_value = mock_knowledge_configs
-
-    response = client.get(f"/marketplace/details/{knowledge_id}")
-
-    assert response.status_code == 200
-    assert len(response.json()) == 2
-    assert response.json() == mock_knowledge_configs
-
-def test_get_marketplace_knowledge_configs_details_not_found(mocker):
-    knowledge_id = 1
 
     # Mock the query function
     mock_db = mocker.patch('superagi.controllers.knowledge_configs.db')
     mock_db.session.query.return_value.filter.return_value.all.return_value = []
 
-    response = client.get(f"/marketplace/details/{knowledge_id}")
-    assert response.status_code == 404
+    response = client.get(f"/knowledge_configs/marketplace/details/{knowledge_id}")
+    assert response.status_code == 200
