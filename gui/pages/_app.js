e1657ccc65f297b7bbc58f62108bb03590829213
NishantBorthakur <nist.16br@gmail.com>
2023-07-25 15:45:22 +0530

    clarity code

--- gui/pages/_app.js
+++ gui/pages/_app.js
@@ -195,6 +195,17 @@ export default function App() {
             `,
         }}
         ></script>
+        {/*Clarity*/}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "i4qee5ixkb");
+          `,
+        }}
+        ></script>
       </Head>
       {showMarketplace && <div className="projectStyle"><MarketplacePublic env={env}/></div>}
       {applicationState === 'AUTHENTICATED' && !showMarketplace ? (<div className="projectStyle">
