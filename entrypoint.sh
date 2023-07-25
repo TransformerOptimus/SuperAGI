97c5ee94ab4045dadb08d1accaf0a19b47f36dad
Fluder-Paradyne <121793617+Fluder-Paradyne@users.noreply.github.com>
2023-07-25 19:24:17 +0530

    Docker image (#836)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@
 # Downloads the tools from marketplace and external tool repositories
 python superagi/tool_manager.py
 
-# Set executable permissions for install_tool_dependencies.sh
-chmod +x install_tool_dependencies.sh
-
 # Install dependencies
 ./install_tool_dependencies.sh
 
