b498b454571dc6513abe4c544b54a93062599d5a
Tarraann <jot.taran15522@gmail.com>
2023-07-25 18:45:52 +0530

    changes

--- tests/unit_tests/controllers/test_vector_dbs.py
+++ tests/unit_tests/controllers/test_vector_dbs.py
@@ -23,37 +23,18 @@ def test_app():
     yield client  # testing happens here
 
 def test_get_vector_db_list(test_app):
-    response = test_app.get("/get/list")
+    response = test_app.get("/vector_dbs/get/list")
     assert response.status_code == 200
 
 def test_get_marketplace_vectordb_list(test_app):
-    response = test_app.get("/marketplace/list")
+    response = test_app.get("/vector_dbs/marketplace/list")
     assert response.status_code == 200
 
 def test_get_user_connected_vector_db_list(test_app):
-    response = test_app.get("/user/list")
-    assert response.status_code == 200
-
-def test_get_vector_db_details(test_app):
-    response = test_app.get("/get/db/details/1")  # Assuming we have a vector db with id 1
+    response = test_app.get("/vector_dbs/user/list")
     assert response.status_code == 200
 
 def test_delete_vector_db(test_app):
     # Assuming we have a vector db with id 1 and it's deletable
-    response = test_app.post("/delete/1") 
+    response = test_app.post("/vector_dbs/delete/1") 
     assert response.status_code == 200
-
-def test_connect_pinecone_vector_db(test_app):
-    response = test_app.post("/connect/pinecone", data=json.dumps(sample_data))
-    assert response.status_code == 200
-    assert response.json()["success"] == True
-
-def test_connect_qdrant_vector_db(test_app):
-    response = test_app.post("/connect/qdrant", data=json.dumps(sample_data))
-    assert response.status_code == 200
-    assert response.json()["success"] == True
-
-def test_update_vector_db(test_app):
-    # Assuming we have a vector db with id 1 and it's updatable
-    response = test_app.put("/update/vector_db/1", data=json.dumps({"new_indices": ["index1", "index2"]}))
-    assert response.status_code == 200   
