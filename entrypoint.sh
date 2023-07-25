1ea8d16050afcac4a94ab9034934cc697d575998
luciferlinx <129729795+luciferlinx101@users.noreply.github.com>
2023-07-25 17:55:13 +0530

    External repo support for marketplace (#847)
    * Added support for parsing marketplace repo
    
    * Added Support for marketplace repo
    
    * Improved Marketplace logic
    
    * Fixed marketplace install
    
    * Handled no path bug
    
    * Updated .gitignore
    
    * Updated unit test
    
    * Added Unit Test
    
    * Updated .gitignore
    
    * Refactored Code
    
    * Updated Marketplace URL
    
    * Added Unit Tests
    
    * Update tool_helper.py
    
    * Update tool_helper.py
    
    ---------
    
    Co-authored-by: I’m <133493246+TransformerOptimus@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Downloads the tools
+# Downloads the tools from marketplace and external tool repositories
 python superagi/tool_manager.py
 
 # Set executable permissions for install_tool_dependencies.sh
