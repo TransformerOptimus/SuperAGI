d0f3a51d213bea3c63c4251453c337049ec862da
Tarraann <jot.taran15522@gmail.com>
2023-07-26 18:30:00 +0530

    knowledge configs changes

--- superagi/controllers/knowledge_configs.py
+++ superagi/controllers/knowledge_configs.py
@@ -2,12 +2,14 @@ from fastapi_sqlalchemy import db
 from fastapi import HTTPException, Depends, Query, status
 from fastapi import APIRouter
 from superagi.config.config import get_config
-from superagi.helper.auth import get_user_organisation
+from superagi.helper.auth import check_auth
 from superagi.models.knowledge_configs import KnowledgeConfigs
+from fastapi_jwt_auth import AuthJWT
 
 router = APIRouter()
 
 @router.get("/marketplace/details/{knowledge_id}")
-def get_marketplace_knowledge_configs_details(knowledge_id: int):
+def get_marketplace_knowledge_configs_details(knowledge_id: int, Authorize: AuthJWT = Depends(check_auth)):
     knowledge_configs = db.session.query(KnowledgeConfigs).filter(KnowledgeConfigs.knowledge_id == knowledge_id).all()
     return knowledge_configs
+
