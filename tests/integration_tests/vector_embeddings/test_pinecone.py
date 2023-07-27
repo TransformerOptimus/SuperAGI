a59a1b644c4578d396c1eeba997efe9631a4dab3
Tarraann <jot.taran15522@gmail.com>
2023-07-27 15:41:10 +0530

    changes in tests

--- tests/integration_tests/vector_embeddings/test_pinecone.py
+++ tests/integration_tests/vector_embeddings/test_pinecone.py
@@ -1,39 +1,26 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from typing import Any
-from superagi.vector_embeddings.pinecone import Pinecone
+from superagi.vector_embeddings.pinecone import Pinecone  
 
-# now, the unittests:
+
 class TestPinecone(unittest.TestCase):
 
-    # assuming that there is a setUp method to set initial vars
     def setUp(self):
-        self.pinecone = Pinecone()
-        self.sample_json = {
-            '1': {"id": "1", "embeds": "embeds1", 'text': 'text1', 'chunk': 'chunk1', 'knowledge_name': 'knowledge1'},
-            '2': {"id": "2", "embeds": "embeds2", 'text': 'text2', 'chunk': 'chunk2', 'knowledge_name': 'knowledge2'}
-        }
+        self.uuid = ["id1", "id2"]
+        self.embeds = ["embed1", "embed2"]
+        self.metadata = ["metadata1", "metadata2"]
+        self.pinecone_instance = Pinecone(self.uuid, self.embeds, self.metadata)
 
-    # testing for the non-empty json
+    def test_init(self):
+        self.assertEqual(self.pinecone_instance.uuid, self.uuid)
+        self.assertEqual(self.pinecone_instance.embeds, self.embeds)
+        self.assertEqual(self.pinecone_instance.metadata, self.metadata)
+    
     def test_get_vector_embeddings_from_chunks(self):
-        result = self.pinecone.get_vector_embeddings_from_chunks(self.sample_json)
-        expected_result = {
-            'vectors': [(
-                "1", 
-                "embeds1", 
-                {'text': 'text1', 'chunk': 'chunk1', 'knowledge_name': 'knowledge1'}
-            ), (
-                "2", 
-                "embeds2", 
-                {'text': 'text2', 'chunk': 'chunk2', 'knowledge_name': 'knowledge2'}
-            )]
+        expected = {
+            'vectors': list(zip(self.uuid, self.embeds, self.metadata))
         }
-        self.assertDictEqual(result, expected_result)
-
-    # testing for an empty json
-    def test_get_vector_embeddings_from_chunks_empty(self):
-        result = self.pinecone.get_vector_embeddings_from_chunks({})
-        self.assertEqual(result, {})
+        result = self.pinecone_instance.get_vector_embeddings_from_chunks()
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
--- tests/unit_tests/vector_embeddings/test_vector_embedding_factory.py
+++ tests/unit_tests/vector_embeddings/test_vector_embedding_factory.py
@@ -1,26 +1,28 @@
 import unittest
 from unittest.mock import patch
-from superagi.types.vector_store_types import VectorStoreType
-import superagi
 from superagi.vector_embeddings.vector_embedding_factory import VectorEmbeddingFactory
 
 class TestVectorEmbeddingFactory(unittest.TestCase):
 
-  @patch('superagi.vector_embeddings.pinecone.Pinecone.__init__', return_value=None) 
-  def test_build_vector_storge_pinecone(self, mock_pinecone):
-      result = VectorEmbeddingFactory.build_vector_storge('PINECONE')
-      mock_pinecone.assert_called_once()
-      self.assertIsInstance(result, superagi.vector_embeddings.pinecone.Pinecone)
+    @patch("superagi.vector_embeddings.pinecone.Pinecone.__init__", return_value=None)
+    @patch("superagi.vector_embeddings.qdrant.Qdrant.__init__", return_value=None)
+    def test_build_vector_storge(self, mock_qdrant, mock_pinecone):
+        test_data = {
+            "1": {"id": 1, "embeds": [1,2,3], "text": "test", "chunk": "chunk", "knowledge_name": "knowledge"},
+            "2": {"id": 2, "embeds": [4,5,6], "text": "test2", "chunk": "chunk2", "knowledge_name": "knowledge2"},
+        }
 
-  @patch('superagi.vector_embeddings.qdrant.Qdrant.__init__', return_value=None)
-  def test_build_vector_storge_qdrant(self, mock_qdrant):
-      result = VectorEmbeddingFactory.build_vector_storge('QDRANT')
-      mock_qdrant.assert_called_once()
-      self.assertIsInstance(result, superagi.vector_embeddings.qdrant.Qdrant)
+        vector_storge = VectorEmbeddingFactory.build_vector_storge('Pinecone', test_data)
 
-  def test_build_vector_storge_invalid(self):
-      with self.assertRaises(ValueError):
-          VectorEmbeddingFactory.build_vector_storge('INVALID')
+        mock_pinecone.assert_called_once_with(
+            [1,2], [[1,2,3],[4,5,6]], [{"text": "test", "chunk": "chunk", "knowledge_name": "knowledge"}, {"text": "test2", "chunk": "chunk2", "knowledge_name": "knowledge2"}]
+        )
 
-if __name__ == '__main__':
+        vector_storge = VectorEmbeddingFactory.build_vector_storge('Qdrant', test_data)
+
+        mock_qdrant.assert_called_once_with(
+            [1,2], [[1,2,3],[4,5,6]], [{"text": "test", "chunk": "chunk", "knowledge_name": "knowledge"}, {"text": "test2", "chunk": "chunk2", "knowledge_name": "knowledge2"}]
+        )
+
+if __name__ == "__main__":
     unittest.main()
