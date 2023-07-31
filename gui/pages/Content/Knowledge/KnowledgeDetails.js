e8a0dc4d4cf19ba81d32de939f10f8814842a905
sayan1101 <139119661+sayan1101@users.noreply.github.com>
2023-07-31 22:27:50 +0530

    Update KnowledgeDetails.js

--- gui/pages/Content/Knowledge/KnowledgeDetails.js
+++ gui/pages/Content/Knowledge/KnowledgeDetails.js
@@ -33,8 +33,8 @@ export default function KnowledgeDetails({internalId, knowledgeId}) {
         .then((response) => {
           console.log(response)
           toast.success("Knowledge uninstalled successfully", {autoClose: 1800});
-           removeTab(knowledgeId, knowledgeName, "Knowledge", internalId);
-           EventBus.emit('reFetchKnowledge', {});
+            removeTab(knowledgeId, knowledgeName, "Knowledge", internalId);
+            EventBus.emit('reFetchKnowledge', {});
         })
         .catch((error) => {
           toast.error("Unable to uninstall knowledge", {autoClose: 1800});
@@ -43,7 +43,7 @@ export default function KnowledgeDetails({internalId, knowledgeId}) {
     } else {
       deleteCustomKnowledge(knowledgeId)
         .then((response) => {
-          toast.success("Knowledge uninstalled successfully", {autoClose: 1800});
+            toast.success("Knowledge uninstalled successfully", {autoClose: 1800});
           removeTab(knowledgeId, knowledgeName, "Knowledge", internalId);
           EventBus.emit('reFetchKnowledge', {});
         })
