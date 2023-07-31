78a06a945c106e5e7fc4b417957f94dbd616ed0f
sayan1101 <139119661+sayan1101@users.noreply.github.com>
2023-07-31 22:21:47 +0530

    Update Knowledge.js

--- gui/pages/Content/Knowledge/Knowledge.js
+++ gui/pages/Content/Knowledge/Knowledge.js
@@ -7,7 +7,7 @@ import {createInternalId} from "@/utils/utils";
 export default function Knowledge({sendKnowledgeData, knowledge}) {
   return (
     <>
-        <div className="container">
+      <div className="container">
         <p className="text_14 mt_8 mb_12 ml_8">Knowledges</p>
         <div className="w_100 mb_10">
           <button className="secondary_button w_100" onClick={() => sendKnowledgeData({
