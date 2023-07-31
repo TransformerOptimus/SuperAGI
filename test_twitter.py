f813326da787bb6d156c12578ba211558033e113
sayan1101 <139119661+sayan1101@users.noreply.github.com>
2023-07-31 16:54:29 +0530

    Delete test_twitter.py

--- test_twitter.py
+++
@@ -1,9 +0,0 @@
-from superagi.helper.twitter_tokens import TwitterTokens
-from superagi.models.db import connect_db
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-engine = connect_db()
-Session = sessionmaker(bind=engine)
-tool_creds = TwitterTokens(Session()).get_twitter_creds(33)
-print(tool_creds)
