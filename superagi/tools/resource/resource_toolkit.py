b82b84dd61ffee1c03b99ed9009adc688b376c15
sayan1101 <139119661+sayan1101@users.noreply.github.com>
2023-08-01 17:24:47 +0530

    Update resource_toolkit.py

--- superagi/tools/resource/resource_toolkit.py
+++ superagi/tools/resource/resource_toolkit.py
@@ -18,9 +18,5 @@ class JiraToolkit(BaseToolkit, ABC):
     def get_env_keys(self) -> List[ToolConfig]:
         return [
             ToolConfig(key="RESOURCE_VECTOR_STORE", key_type=ToolConfigKeyType.STRING, is_required= True, is_secret = True),
-            ToolConfig(key="RESOURCE_VECTOR_STORE_INDEX_NAME", key_type=ToolConfigKeyType.STRING, is_required=True, is_secret=True),
-            ToolConfig(key="TEST_KEY_HELLO_1", key_type=ToolConfigKeyType.FILE, is_required = False, is_secret = False),
-            ToolConfig(key="TEST_KEY_HELLO_2"),
-            "TEST_KEY_HELLO_3"
-            
+            ToolConfig(key="RESOURCE_VECTOR_STORE_INDEX_NAME", key_type=ToolConfigKeyType.STRING, is_required=True, is_secret=True)
         ]
