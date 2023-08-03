4e9891cf1bc542823ebcea8428bd58abeb6085af
Tarraann <jot.taran15522@gmail.com>
2023-08-03 19:33:45 +0530

    Oauth and Update Button handled

--- gui/pages/Content/Toolkits/ToolkitWorkspace.js
+++ gui/pages/Content/Toolkits/ToolkitWorkspace.js
@@ -78,6 +78,7 @@ export default function ToolkitWorkspace({env, toolkitDetails, internalId}) {
   };
 
   const handleAuthenticateClick = async (toolkitName) => {
+    handleUpdateChanges();
     if (toolkitName === "Google Calendar Toolkit") {
       authenticateGoogleCred(toolkitDetails.id)
         .then((response) => {
