9e3533cf62ef70e83ffa16fe7c64b47149dc7b04
Adarshjha619 <kumar.adarsh51@gmail.com>
2023-08-04 16:00:29 +0530

    changes

--- superagi/tools/file/read_file.py
+++ superagi/tools/file/read_file.py
@@ -1,7 +1,77 @@
+# import os
+# from typing import Type, Optional
+
+# from pydantic import BaseModel, Field
+
+# from superagi.helper.resource_helper import ResourceHelper
+# from superagi.helper.s3_helper import S3Helper
+# from superagi.models.agent_execution import AgentExecution
+# from superagi.resource_manager.file_manager import FileManager
+# from superagi.tools.base_tool import BaseTool
+# from superagi.models.agent import Agent
+# from superagi.types.storage_types import StorageType
+# from superagi.config.config import get_config
+
+
+# class ReadFileSchema(BaseModel):
+#     """Input for CopyFileTool."""
+#     file_name: str = Field(..., description="Path of the file to read")
+
+
+# class ReadFileTool(BaseTool):
+#     """
+#     Read File tool
+
+#     Attributes:
+#         name : The name.
+#         description : The description.
+#         args_schema : The args schema.
+#     """
+#     name: str = "Read File"
+#     agent_id: int = None
+#     agent_execution_id: int = None
+#     args_schema: Type[BaseModel] = ReadFileSchema
+#     description: str = "Reads the file content in a specified location"
+#     resource_manager: Optional[FileManager] = None
+
+#     def _execute(self, file_name: str):
+#         """
+#         Execute the read file tool.
+
+#         Args:
+#             file_name : The name of the file to read.
+
+#         Returns:
+#             The file content and the file name
+#         """
+#         final_path = ResourceHelper.get_agent_read_resource_path(file_name, agent=Agent.get_agent_from_id(
+#             session=self.toolkit_config.session, agent_id=self.agent_id), agent_execution=AgentExecution
+#                                                                  .get_agent_execution_from_id(session=self
+#                                                                                               .toolkit_config.session,
+#                                                                                               agent_execution_id=self
+#                                                                                               .agent_execution_id))
+#         if StorageType.get_storage_type(get_config("STORAGE_TYPE", StorageType.FILE.value)) == StorageType.S3:
+#             return S3Helper().read_from_s3(final_path)
+
+#         if final_path is None or not os.path.exists(final_path):
+#             raise FileNotFoundError(f"File '{file_name}' not found.")
+#         directory = os.path.dirname(final_path)
+#         os.makedirs(directory, exist_ok=True)
+
+#         with open(final_path, 'r') as file:
+#             file_content = file.read()
+#         max_length = len(' '.join(file_content.split(" ")[:1000]))
+#         return file_content[:max_length] + "\n File " + file_name + " read successfully."
+
+
 import os
 from typing import Type, Optional
+import ebooklib
+import bs4 
+from bs4 import BeautifulSoup
 
 from pydantic import BaseModel, Field
+from ebooklib import epub
 
 from superagi.helper.resource_helper import ResourceHelper
 from superagi.helper.s3_helper import S3Helper
@@ -11,7 +81,7 @@ from superagi.tools.base_tool import BaseTool
 from superagi.models.agent import Agent
 from superagi.types.storage_types import StorageType
 from superagi.config.config import get_config
-
+from unstructured.partition.auto import partition
 
 class ReadFileSchema(BaseModel):
     """Input for CopyFileTool."""
@@ -57,10 +127,23 @@ class ReadFileTool(BaseTool):
             raise FileNotFoundError(f"File '{file_name}' not found.")
         directory = os.path.dirname(final_path)
         os.makedirs(directory, exist_ok=True)
+        
+        # Check if the file is an .epub file
+        if final_path.lower().endswith('.epub'):
+            # Use ebooklib to read the epub file
+            book = epub.read_epub(final_path)
+            # Get the text content from each item in the book
+            content = []
+            for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+                soup = BeautifulSoup(item.get_content(), 'html.parser')
+                content.append(soup.get_text())
 
-        with open(final_path, 'r') as file:
-            file_content = file.read()
-        max_length = len(' '.join(file_content.split(" ")[:1000]))
-        return file_content[:max_length] + "\n File " + file_name + " read successfully."
+            content = "\n".join(content)
+        else:
+            elements = partition(final_path)
+            content = "\n\n".join([str(el) for el in elements])
+   
+        return content
+    
 
 
