b581735a4620d92272de8562b0996149f6da40f0
Tarraann <jot.taran15522@gmail.com>
2023-08-09 18:25:03 +0530

    Dimensions change for weaviate

--- superagi/controllers/vector_dbs.py
+++ superagi/controllers/vector_dbs.py
@@ -77,7 +77,7 @@ def connect_pinecone_vector_db(data: dict, organisation = Depends(get_user_organ
     pinecone_db = Vectordbs.add_vector_db(db.session, data["name"], "Pinecone", organisation)
     VectordbConfigs.add_vector_db_config(db.session, pinecone_db.id, db_creds)
     for collection in data["collections"]:
-        VectordbIndices.add_vector_index(db.session, collection, pinecone_db.id, db_connect_for_index["dimensions"], index_state)
+        VectordbIndices.add_vector_index(db.session, collection, pinecone_db.id, index_state, db_connect_for_index["dimensions"])
     return {"id": pinecone_db.id, "name": pinecone_db.name}
 
 @router.post("/connect/qdrant")
@@ -97,7 +97,7 @@ def connect_qdrant_vector_db(data: dict, organisation = Depends(get_user_organis
     qdrant_db = Vectordbs.add_vector_db(db.session, data["name"], "Qdrant", organisation)
     VectordbConfigs.add_vector_db_config(db.session, qdrant_db.id, db_creds)
     for collection in data["collections"]:
-        VectordbIndices.add_vector_index(db.session, collection, qdrant_db.id, db_connect_for_index["dimensions"], index_state)
+        VectordbIndices.add_vector_index(db.session, collection, qdrant_db.id, index_state, db_connect_for_index["dimensions"])
     
     return {"id": qdrant_db.id, "name": qdrant_db.name}
 
@@ -117,7 +117,7 @@ def connect_weaviate_vector_db(data: dict, organisation = Depends(get_user_organ
     weaviate_db = Vectordbs.add_vector_db(db.session, data["name"], "Weaviate", organisation)
     VectordbConfigs.add_vector_db_config(db.session, weaviate_db.id, db_creds)
     for collection in data["collections"]:
-        VectordbIndices.add_vector_index(db.session, collection, weaviate_db.id, data["dimensions"], index_state)
+        VectordbIndices.add_vector_index(db.session, collection, weaviate_db.id, index_state)
 
     return {"id": weaviate_db.id, "name": weaviate_db.name}
 
@@ -141,7 +141,7 @@ def update_vector_db(new_indices: list, vector_db_id: int):
             index_state = "Custom" if vector_db_index_stats["vector_count"] > 0 else "None"
         except:
            raise HTTPException(status_code=400, detail="Unable to update vector db")
-        VectordbIndices.add_vector_index(db.session, index, vector_db_id, vector_db_index_stats["dimensions"], index_state)
+        VectordbIndices.add_vector_index(db.session, index, vector_db_id, index_state, vector_db_index_stats["dimensions"])
 
 
 
