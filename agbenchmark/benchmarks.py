61446adfff2870fc3aee7ec0e83583e5ad9a37b1
Fluder-Paradyne <121793617+Fluder-Paradyne@users.noreply.github.com>
2023-08-16 17:41:56 +0530

    Update benchmarks.py
    poc working

--- agbenchmark/benchmarks.py
+++ agbenchmark/benchmarks.py
@@ -7,51 +7,54 @@ import os
 baseUrl = "http://localhost:3000/api"
 
 
-def run_specific_agent(task: str, pused_payload=None) -> None:
+def run_specific_agent(task: str) -> None:
     # create and start the agent here and dynamically pass in the task
     # must have File Toolkit, Search Toolkit minimum
 
     headers = {"Content-Type": "application/json"}
 
-    # EXAMPLE FROM BEFORE
     payload = {
-        "name": "Agent",
-        "project_id": 1,  # project id goes here
-        "description": "AI assistant to solve complex problems",
-        "goal": [
-            "Please fulfill the instructions you are given to the best of your ability. Make sure to output relevant information into the workspace."
+        'status': 'Running',
+        'name': 'agent',
+        'project_id': 1,
+        'description': 'AI assistant to solve complex problems',
+        'goal': [
+            f"{task}"
         ],
-        "instruction": [f"{task}"],
-        "agent_type": "Don't Maintain Task Queue",
-        "constraints": [
+        'instruction': ['Please fulfill the goals you are given to the best of your ability. Make sure to output relevant information into the workspace.'],
+        'agent_workflow': 'Goal Based Workflow',
+        'constraints': [
             "~4000 word limit for short term memory.",
             "Your short term memory is short, so immediately save important information to files.",
             "If you are unsure how you previously did something or want to recall past events, thinking about similar events will help you remember.",
             "No user assistance",
             'Exclusively use the commands listed in double quotes e.g. "command name"',
         ],
-        "toolkits": [],
-        "tools": [
+        'toolkits': [],
+        'tools': [
             1,
-            7,
-            8,
-            9,
-            10,
-            11,
-            19,
-        ],  # CodingTool, Append File, Delete File, List File, Read File, Write File, GoogleSearch
-        "exit": "Exit strategy",
-        "iteration_interval": 500,
-        "model": "gpt-3.5-turbo",
-        "permission_type": "God Mode",
-        "LTM_DB": "Pinecone",
-        "memory_window": 10,
-        "max_iterations": 100,
+            21,
+            22,
+            25
+        ],
+        'exit': 'No exit criterion',
+        'iteration_interval': 500,
+        'model': 'gpt-3.5-turbo-16k',
+        'max_iterations': 25,
+        'permission_type': 'God Mode',
+        'LTM_DB': 'Pinecone',
+        'user_timezone': 'Asia/Kolkata',
+        'knowledge': None,
     }
 
     response = requests.request(
         "POST", f"{baseUrl}/agents/create", headers=headers, data=json.dumps(payload)
     )
+
+    agent_execution_id = response.json()['execution_id']
+    _ = requests.request(
+        "PUT", f"{baseUrl}/agentexecutions/update/{agent_execution_id}", headers=headers,
+        data=json.dumps({'status': 'RUNNING'}))
 
     with open("agbenchmark/config.json", "r") as f:
         config = json.load(f)
@@ -59,17 +62,15 @@ def run_specific_agent(task: str, pused_payload=None) -> None:
     # Update the output workspace path depending on agentexecution and agentid
     print(response.json(), 'response')
     response = response.json()
-    output_path = f"workspace/output/{response['id']}/{response['execution_id']}"
-    input_path= f"workspace/input/{response['id']}/{response['execution_id']}"
+    output_path = f"workspace/output/"
+    input_path = f"workspace/input/"
     config["workspace"]["output"] = output_path
     config["workspace"]["input"] = input_path
-    print(config, 'configerino')
     # config["workspace"]["output"] = os.path.join(
     #     "workspace","output", f"{response['id']}", f"{response['execution_id']}")
     #
     # config["workspace"]["input"] = os.path.join(
     #     "workspace","input", f"{response['id']}", f"{response['execution_id']}")
-
 
     with open("agbenchmark/config.json", "w") as f:
         json.dump(config, f)
@@ -86,7 +87,7 @@ def run_specific_agent(task: str, pused_payload=None) -> None:
 
         if time.time() - start_time > 60:
             response = requests.request(
-                "POST", f"{baseUrl}/agentexecutions/update/{response['execution_id']}", headers=headers,
+                "PUT", f"{baseUrl}/agentexecutions/update/{response['execution_id']}", headers=headers,
                 data=json.dumps(pause_payload)
             )
             break
@@ -100,4 +101,5 @@ if __name__ == "__main__":
         print("Usage: python script.py <task>")
         sys.exit(1)
     task = sys.argv[1]
+    print(sys.argv)
     run_specific_agent(task)
