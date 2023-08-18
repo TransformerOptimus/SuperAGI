3805f993f153cc61daf97762e11a66ca735fd90d
Kalki <parth.pathak@science.christuniversity.in>
2023-08-18 16:27:49 +0530

    Updates

--- superagi/llms/openai.py
+++ superagi/llms/openai.py
@@ -63,8 +63,6 @@ class OpenAi(BaseLlm):
         """
         try:
             # openai.api_key = get_config("OPENAI_API_KEY")
-            print(max_tokens,'ssssssssssssss')
-            print(messages)
             response = openai.ChatCompletion.create(
                 n=self.number_of_results,
                 model=self.model,
@@ -76,8 +74,6 @@ class OpenAi(BaseLlm):
                 presence_penalty=self.presence_penalty
             )
             content = response.choices[0].message["content"]
-            print("nnnnnnnnnnnnnnnnnnnnnnn")
-            print(content)
             return {"response": response, "content": content}
         except AuthenticationError as auth_error:
             logger.info("OpenAi AuthenticationError:", auth_error)
