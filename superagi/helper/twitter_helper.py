2d9d5a63d1b6ac75a65e1318a596e5c07192ca25
abhijeet <abhijeet@contlo.com>
2023-08-22 15:47:46 +0530

    Updated logs'

--- superagi/helper/twitter_helper.py
+++ superagi/helper/twitter_helper.py
@@ -41,9 +41,9 @@ class TwitterHelper:
         return final_path
 
     def _get_image_data(self, file_path):
-        print("LOG TEST : FILE PATH ",file_path)
+        print("______________________________________________LOG TEST : FILE PATH _______________________ ",file_path)
         if StorageType.get_storage_type(get_config("STORAGE_TYPE", StorageType.FILE.value)) == StorageType.S3:
-                print("LOG TEST : STORAGE TYPE IS S3")
+                print("_______________________________________________LOG TEST : STORAGE TYPE IS S3________________")
                 attachment_data = S3Helper().read_binary_from_s3(file_path)
         else:
             with open(file_path, "rb") as file:
