9593ce9caa8501a1d211aa62017df973b9b76c74
Kalki <parth.pathak@science.christuniversity.in>
2023-08-22 18:46:11 +0530

    Updates

--- gui/pages/Dashboard/Settings/Model.js
+++ gui/pages/Dashboard/Settings/Model.js
@@ -13,7 +13,7 @@ export default function Model({organisationId}) {
     {'name':'Open AI API key','logo':'/images/openai_logo.svg','source':'OpenAi', 'api_key': ''},
     {'name':'Hugging Face auth token','logo':'/images/huggingface_logo.svg','source':'Hugging Face', 'api_key': ''},
     {'name':'Replicate auth token','logo':'/images/replicate_logo.svg','source':'Replicate', 'api_key': ''},
-    {'name':'Google AI API key','logo':'/images/google_palm_logo.svg','source':'Google Palm', 'api_key': ''}
+    {'name':'Google API key','logo':'/images/google_palm_logo.svg','source':'Google Palm', 'api_key': ''}
   ]);
   const [updatedModels, setUpdatedModels] = useState([]);
 
