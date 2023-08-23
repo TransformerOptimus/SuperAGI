fdb4154b740045722d9408295b10a98e81453159
Kalki <parth.pathak@science.christuniversity.in>
2023-08-23 18:26:39 +0530

    Updates

--- tests/unit_tests/llms/test_model_factory.py
+++ tests/unit_tests/llms/test_model_factory.py
@@ -1,35 +1,52 @@
-# import unittest
-# from superagi.llms.llm_model_factory import get_model
-# from superagi.llms.google_palm import GooglePalm
+# from unittest.mock import MagicMock, patch
 # from superagi.llms.openai import OpenAi
 # from superagi.llms.replicate import Replicate
-# from superagi.llms.hugging_face import HuggingFace
-# from unittest.mock import patch, MagicMock, create_autospec
-# from sqlalchemy.orm import Session
+# from superagi.models.models_config import ModelsConfig
+# from superagi.models.models import Models
+# from superagi.models.db import connect_db
+# from sqlalchemy.orm import sessionmaker
+# import pytest
 #
-# class TestGetModel(unittest.TestCase):
-#     @patch('superagi.llms.llm_model_factory.connect_db')
-#     def test_get_model(self, mock_connect_db):
-#         mock_session = MagicMock()
-#         mock_connect_db().Session().return_value = mock_session
 #
-#         mock_model_instance = MagicMock()
-#         mock_model_instance.model_name = "gpt-3.5-turbo"
-#         mock_model_instance.model_provider_id = 1
-#         mock_model_instance.version = "1.0.0"
-#         mock_model_instance.end_point = "/api/models"
+# @pytest.fixture
+# def mock_db_session():
+#     db_session = MagicMock()
+#     db_session.query().filter().first().return_value = Models(model_name="gpt-3.5-turbo", org_id=1, model_provider_id=1)
+#     db_session.query().filter().first().return_value = ModelsConfig(provider="OpenAI")
+#     return db_session
 #
-#         mock_provider_response = MagicMock()
-#         mock_provider_response.provider = 'OpenAI'
 #
-#         mock_query = MagicMock()
-#         mock_query.filter().first.side_effect = [mock_model_instance, mock_provider_response]
-#         mock_session.query.return_value = mock_query
+# @patch("superagi.models.db.connect_db")
+# @patch("sqlalchemy.orm.sessionmaker")
+# def test_get_model_openai(mock_sessionmaker, mock_connect_db, mock_db_session):
+#     mock_sessionmaker.return_value = MagicMock(return_value=mock_db_session)
+#     mock_connect_db.return_value = MagicMock()
 #
-#         result = get_model("org_123", "api_key_123", model="gpt-3.5-turbo")
+#     from superagi.llms.openai import OpenAi
+#     result = OpenAi.get_model(organisation_id=1, api_key="TEST_KEY")
 #
-#         self.assertIsInstance(result, OpenAi)
-#         self.assertEqual(result.model, "gpt-3.5-turbo")
-#
-# if __name__ == "__main__":
-#     unittest.main()
+#     assert isinstance(result, OpenAi)
+# #
+# # @patch('superagi.models.db.connect_db')
+# # @patch('superagi.llms.replicate.Replicate')
+# # def test_get_model_replicate(mock_replicate, mock_db, mock_session_maker):
+# #     mock_session_maker.query().filter().filter().first().return_value.provider = 'Replicate'
+# #     from superagi.models.db import get_model
+# #     result = get_model(organisation_id=1, api_key="TEST_KEY")
+# #     assert isinstance(result, superagi.llms.replicate.Replicate)
+# #
+# # @patch('superagi.models.db.connect_db')
+# # @patch('superagi.llms.google_palm.GooglePalm')
+# # def test_get_model_google_palm(mock_google_palm, mock_db, mock_session_maker):
+# #     mock_session_maker.query().filter().filter().first().return_value.provider = 'Google Palm'
+# #     from superagi.models.db import get_model
+# #     result = get_model(organisation_id=1, api_key="TEST_KEY")
+# #     assert isinstance(result, superagi.llms.google_palm.GooglePalm)
+# #
+# # @patch('superagi.models.db.connect_db')
+# # @patch('superagi.llms.hugging_face.HuggingFace')
+# # def test_get_model_hugging_face(mock_hugging_face, mock_db, mock_session_maker):
+# #     mock_session_maker.query().filter().filter().first().return_value.provider = 'Hugging Face'
+# #     from superagi.models.db import get_model
+# #     result = get_model(organisation_id=1, api_key="TEST_KEY")
+# #     assert isinstance(result, superagi.llms.hugging_face.HuggingFace)
