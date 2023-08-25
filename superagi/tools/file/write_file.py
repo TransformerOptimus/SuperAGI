8b0135743860cc0942c32eb64ccfe23bcee531cc
Arkajit Datta <61142632+Arkajit-Datta@users.noreply.github.com>
2023-08-25 15:49:05 +0530

    PDF and DOCX support in Write File - Feature Improvement, close #548 (#928)
    * Added functions to write various file types and a file handler too
    
    * FileManager updated to handle and save HTMLs.
    
    * adding PDF + DOCX support to save images
    
    * Added Wkhtmltopdf package installation run commands in docker
    
    * Added get_all_responses feature for extractng the response for particular tools
    
    * Added Image embedding feature, this will extract and embed the images generated by the agent during the run
    
    * renaming functions and refactoring
    
    * renaming functions and refactoring
    
    * Update Dockerfile
    
    * removing unsused classmethods
    
    * Finding generated images and attached files in the write tool. Images are fetched inorder to be embedded in the respective file type.
    
    * Adding the filename and paths to the Resource Manager S3 storage
    
    * Code Cleanup
    
    * added logger: Fix for the failing TEST
    
    ---------
    
    Co-authored-by: Fluder-Paradyne <121793617+Fluder-Paradyne@users.noreply.github.com>

--- superagi/tools/file/write_file.py
+++ superagi/tools/file/write_file.py
@@ -1,20 +1,23 @@
 from typing import Type, Optional
+import base64
+import os
 
 from pydantic import BaseModel, Field
 
-# from superagi.helper.s3_helper import upload_to_s3
+from superagi.helper.prompt_reader import PromptReader
+from superagi.helper.resource_helper import ResourceHelper
 from superagi.resource_manager.file_manager import FileManager
 from superagi.tools.base_tool import BaseTool
-
-
-# from superagi.helper.s3_helper import upload_to_s3
+from superagi.tools.tool_response_query_manager import ToolResponseQueryManager
+from superagi.llms.base_llm import BaseLlm
+from superagi.models.agent import Agent
+from superagi.models.agent_execution import AgentExecution
 
 
 class WriteFileInput(BaseModel):
     """Input for CopyFileTool."""
     file_name: str = Field(..., description="Name of the file to write. Only include the file name. Don't include path.")
     content: str = Field(..., description="File content to write")
-
 
 class WriteFileTool(BaseTool):
     """
@@ -27,12 +30,13 @@ class WriteFileTool(BaseTool):
         args_schema : The args schema.
         resource_manager: File resource manager.
     """
+    llm: Optional[BaseLlm] = None
     name: str = "Write File"
     args_schema: Type[BaseModel] = WriteFileInput
-    description: str = "Writes text to a file"
-    agent_id: int = None
+    description: str = "Writes content in a file. The content can carry text and images."
     resource_manager: Optional[FileManager] = None
-
+    agent_id:int =None
+    agent_execution_id:int =None
     class Config:
         arbitrary_types_allowed = True
 
@@ -47,4 +51,125 @@ class WriteFileTool(BaseTool):
         Returns:
             success message if message is file written successfully or failure message if writing file fails.
         """
-        return self.resource_manager.write_file(file_name, content)
+        
+        attached_files = self._get_attached_files()
+        file_type = file_name.split('.')[-1].lower()
+        
+        if file_type not in ['pdf', 'docx', 'doc']:
+            return self.resource_manager.write_file(file_name, content)
+        
+        html_code_content = self._convert_content_into_html(content=content, attached_files = attached_files,formated_for=file_type)
+        
+        return self.resource_manager.write_file(file_name, html_code_content)
+
+    def _convert_content_into_html(self, content: str, attached_files: list, formated_for: str) -> str:
+        """
+        Converts the content into an HTML file
+        Args:
+            content (str): Content to be beautified and formatted
+            formated_for (str): HTML content to be specifically formatted for a specific document type
+        Returns:
+            HTML Code (str): HTML code of the formated content 
+        """
+        prompt = PromptReader.read_tools_prompt(__file__, "content_to_html_prompt.txt")
+        prompt = prompt.replace("{content}", content)
+
+        if image_file_paths := self._get_file_path_of_images(attached_files=attached_files):
+            embedding_image_prompt = PromptReader.read_tools_prompt(__file__, "add_images_to_html.txt")
+            for idx, image_path in enumerate(image_file_paths):
+                embedding_image_prompt += f"\n{idx+1}. {image_path}"
+            prompt = prompt.replace("{embedding_image}", embedding_image_prompt)
+        else:
+            prompt = prompt.replace("{embedding_image}", "")
+            
+        messages = [{"role": "system", "content": prompt}]
+        result = self.llm.chat_completion(messages, max_tokens=self.max_token_limit)
+        
+        if formated_for == 'pdf':
+            return self._html_formatting_for_pdf(content=result["content"], image_list=image_file_paths)
+        return result['content']
+    
+    def _get_file_path_of_images(self, attached_files: list):
+        """
+        Filters Images from the attached files list and finds out the corresponding paths. 
+
+        Args:
+            attached_files: List of names of files generated
+
+        Returns:
+            Full paths of the image files
+        """
+        image_extensions, image_paths = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp'],[]
+        for file_name in attached_files:
+            path = self._get_file_path(file_name=file_name)
+            if not os.path.exists(path):
+                continue
+            _, file_extension = os.path.splitext(path)
+            # Check if the file extension is in the list of image extensions
+            if file_extension.lower() in image_extensions:
+                image_paths.append(path)
+        return image_paths
+    
+    def _get_file_path(self, file_name: str) -> list:
+        """
+        Gets the path of the file
+
+        Args:
+            file_name: Name of file 
+
+        Returns:
+            Full paths of the file
+        """
+        session = self.toolkit_config.session
+
+        return ResourceHelper().get_agent_read_resource_path(
+            file_name,
+            agent=Agent.get_agent_from_id(session, self.agent_id),
+            agent_execution=AgentExecution.get_agent_execution_from_id(
+                session, self.agent_execution_id
+            ),
+        )
+    
+    def _image_to_base64(self, image_path):
+        with open(image_path, "rb") as image_file:
+            encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
+        return encoded_image
+    
+    def _html_formatting_for_pdf(self, content: str, image_list: list) -> str:
+        """
+        Converts image paths into base64 inputs in an HTML file
+        Args:
+            content (str): HTML code
+            image_list (list): List of images to be converted and replaced
+        Returns:
+            HTML Code (str): Formatted HTML code 
+        """
+        
+        for image_path in image_list:
+            content = content.replace(f"{image_path}", f"data:image/png;base64,{self._image_to_base64(image_path=image_path)}")
+        return content
+
+    def _get_attached_files(self):
+        output_directory = ResourceHelper.get_root_output_dir()
+        if "{agent_id}" not in output_directory:
+            return []
+        output_directory = ResourceHelper.get_formatted_agent_level_path(agent=Agent
+                                                                        .get_agent_from_id(session=self
+                                                                                           .toolkit_config.session,
+                                                                                           agent_id=self.agent_id),
+                                                                        path=output_directory)
+        agent_execution=AgentExecution.get_agent_execution_from_id(session=self.toolkit_config.session, agent_execution_id=self.agent_execution_id)
+        if agent_execution is not None and "{agent_execution_id}" in output_directory:
+            output_directory = ResourceHelper.get_formatted_agent_execution_level_path(agent_execution=agent_execution, path=output_directory)
+
+        return self._list_files(output_directory)
+    
+    def _list_files(self, directory):
+        found_files = []
+        for root, dirs, files in os.walk(directory):
+            found_files.extend(
+                file
+                for file in files
+                if not file.startswith(".") and "__pycache__" not in root
+            )
+        return found_files
