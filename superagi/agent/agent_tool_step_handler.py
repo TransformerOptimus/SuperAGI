1d5d06644aa4adf6b9e0324f6fa695087b298417
Kalki <97698934+jedan2506@users.noreply.github.com>
2023-08-25 11:24:26 +0530

    Models superagi (#1117)

--- superagi/agent/agent_tool_step_handler.py
+++ superagi/agent/agent_tool_step_handler.py
@@ -121,7 +121,7 @@ class AgentToolStepHandler:
         if tool_name == "QueryResourceTool":
             resource_summary = ResourceSummarizer(session=self.session,
                                                   agent_id=self.agent_id,
-                                                  model= agent_config["model"]).fetch_or_create_agent_resource_summary(
+                                                  model=agent_config["model"]).fetch_or_create_agent_resource_summary(
                 default_summary=agent_config.get("resource_summary"))
 
         organisation = Agent.find_org_by_agent_id(self.session, self.agent_id)
