c973ca96c562b83670d9c1b43a913a2b8e40d3db
Tarraann <jot.taran15522@gmail.com>
2023-08-30 13:34:23 +0530

    removed refactoring

--- superagi/models/db.py
+++ superagi/models/db.py
@@ -19,8 +19,20 @@ def connect_db():
         return engine
 
     # Create the connection URL
-    db_url = get_db_url()
+    db_host = get_config('DB_HOST', 'super__postgres')
+    db_username = get_config('DB_USERNAME')
+    db_password = get_config('DB_PASSWORD')
+    db_name = get_config('DB_NAME')
+    db_url = get_config('DB_URL', None)
 
+    if db_url is None:
+        if db_username is None:
+            db_url = f'postgresql://{db_host}/{db_name}'
+        else:
+            db_url = f'postgresql://{db_username}:{db_password}@{db_host}/{db_name}'
+    else:
+        db_url = urlparse(db_url)
+        db_url = db_url.scheme + "://" + db_url.netloc + db_url.path
     # Create the SQLAlchemy engine
     engine = create_engine(db_url,
                            pool_size=20,  # Maximum number of database connections in the pool
