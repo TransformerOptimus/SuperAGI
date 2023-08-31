927e29bf54f4353015ee327281cb6efaa5d5dece
James Wakelim <james.wakelim@neutralaiz.com>
2023-08-31 00:23:32 -0400

    Added functionaility to use APT packages

--- install_tool_dependencies.sh
+++ install_tool_dependencies.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Update and upgrade apt settings and apps
+apt update && apt upgrade -y
+xargs apt install -y < /app/requirements_apt.txt
+
 # Run the project's main requirements.txt
 pip install -r /app/requirements.txt
 
-# Loop through the tools directories and install their requirements.txt if they exist
 for tool in /app/superagi/tools/* /app/superagi/tools/external_tools/* /app/superagi/tools/marketplace_tools/* ; do
+# Loop through the tools directories and install their apt_requirements.txt if they exist
+  if [ -d "$tool" ] && [ -f "$tool/requirements_apt.txt" ]; then
+    echo "Installing apt requirements for tool: $(basename "$tool")"
+    xargs apt install -y < "$tool/requirements_apt.txt"
+  fi
+# Loop through the tools directories and install their requirements.txt if they exist
   if [ -d "$tool" ] && [ -f "$tool/requirements.txt" ]; then
     echo "Installing requirements for tool: $(basename "$tool")"
     pip install -r "$tool/requirements.txt"
