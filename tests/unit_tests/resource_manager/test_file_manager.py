a8ba439e863d67cd456b1f5d63c19141f851831b
Abhijeet <129729795+luciferlinx101@users.noreply.github.com>
2023-09-12 16:17:37 +0530

    Revert "Revert "PDF and DOCX support in Write File - Feature Improvement, close #548 (#1125)" (#1202)"
    This reverts commit 3153def135b786477d0aebe1bf46f93f2724b214.

--- tests/unit_tests/resource_manager/test_file_manager.py
+++ tests/unit_tests/resource_manager/test_file_manager.py
@@ -26,7 +26,7 @@ def test_write_binary_file(resource_manager):
         logger_mock.assert_called_once_with("Binary test.png saved successfully")
 
 
-def test_write_file(resource_manager):
+def test_write_txt_file(resource_manager):
     with patch.object(ResourceHelper, 'get_resource_path', return_value='test_path'), \
             patch.object(ResourceHelper, 'make_written_file_resource',
                          return_value=Resource(name='test.txt', storage_type='S3')), \
@@ -35,3 +35,33 @@ def test_write_file(resource_manager):
         result = resource_manager.write_file('test.txt', 'content')
         assert result == "test.txt - File written successfully"
         logger_mock.assert_called_once_with("test.txt - File written successfully")
+
+def test_write_pdf_file(resource_manager):
+    with patch.object(ResourceHelper, 'get_resource_path', return_value='test_path'), \
+            patch.object(ResourceHelper, 'make_written_file_resource',
+                         return_value=Resource(name='test.pdf', storage_type='S3')), \
+            patch.object(S3Helper, 'upload_file'), \
+            patch.object(logger, 'info') as logger_mock:
+        result = resource_manager.write_file('test.pdf', 'content')
+        assert result == "test.pdf - File written successfully"
+        logger_mock.assert_called_once_with("test.pdf - File written successfully")
+        
+def test_write_docx_file(resource_manager):
+    with patch.object(ResourceHelper, 'get_resource_path', return_value='test_path'), \
+            patch.object(ResourceHelper, 'make_written_file_resource',
+                         return_value=Resource(name='test.docx', storage_type='S3')), \
+            patch.object(S3Helper, 'upload_file'), \
+            patch.object(logger, 'info') as logger_mock:
+        result = resource_manager.write_file('test.docx', 'content')
+        assert result == "test.docx - File written successfully"
+        logger_mock.assert_called_once_with("test.docx - File written successfully")
+        
+def test_write_csv_file(resource_manager):
+    with patch.object(ResourceHelper, 'get_resource_path', return_value='test_path'), \
+            patch.object(ResourceHelper, 'make_written_file_resource',
+                         return_value=Resource(name='test.csv', storage_type='S3')), \
+            patch.object(S3Helper, 'upload_file'), \
+            patch.object(logger, 'info') as logger_mock:
+        result = resource_manager.write_file('test.csv', 'content')
+        assert result == "test.csv - File written successfully"
+        logger_mock.assert_called_once_with("test.csv - File written successfully")
