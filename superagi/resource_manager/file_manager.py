dd37a24500d2501ca1f2527d8e6da4f3a84ab677
Conor Hovis <conorhovis1@gmail.com>
2023-10-24 23:52:47 -0500

    Refactored the `FileManager` class to include methods for writing binary, file, and CSV data, as well as reading files and getting a list of files.

--- superagi/resource_manager/file_manager.py
+++ superagi/resource_manager/file_manager.py
@@ -330,3 +330,198 @@ class FileManager:
             logger.error(f"Error while accessing files in {final_path}: {err}")
             files = []
         return files
+    # entire file content ...
+# ... goes in between
+```
+
+package.json
+```
+// entire file content ...
+// ... goes in between
+```
+
+gui/package-lock.json
+```
+// entire file content ...
+// ... goes in between
+```
+
+requirements.txt
+```
+// entire file content ...
+// ... goes in between
+superagi/resource_manager/file_manager.py
+```python
+superagi/resource_manager/file_manager.py
+```
+```python
+import csv
+import os
+from typing import Any, Optional
+from sqlalchemy.orm import Session
+from superagi.config.config import get_config
+from superagi.helper.resource_helper import ResourceHelper
+from superagi.helper.s3_helper import S3Helper
+from superagi.lib.logger import logger
+from superagi.models.agent import Agent
+from superagi.models.agent_execution import AgentExecution
+from superagi.types.storage_types import StorageType
+
+class FileManager:
+    def __init__(self, session: Session, agent_id: Optional[int] = None, agent_execution_id: Optional[int] = None):
+        self.session = session
+        self.agent_id = agent_id
+        self.agent_execution_id = agent_execution_id
+
+    def write_binary_file(self, file_name: str, data: bytes) -> str:
+        if self.agent_id is not None:
+            final_path = ResourceHelper.get_agent_write_resource_path(
+                file_name,
+                Agent.get_agent_from_id(self.session, self.agent_id),
+                AgentExecution.get_agent_execution_from_id(self.session, self.agent_execution_id)
+            )
+        else:
+            final_path = ResourceHelper.get_resource_path(file_name)
+        try:
+            with open(final_path, mode="wb") as img:
+                img.write(data)
+            self.write_to_s3(file_name, final_path)
+            logger.info(f"Binary {file_name} saved successfully")
+            return f"Binary {file_name} saved successfully"
+        except Exception as err:
+            return f"Error write_binary_file: {err}"
+
+    def write_to_s3(self, file_name: str, final_path: str) -> None:
+        with open(final_path, 'rb') as img:
+            resource = ResourceHelper.make_written_file_resource(
+                file_name=file_name,
+                agent=Agent.get_agent_from_id(self.session, self.agent_id),
+                agent_execution=AgentExecution.get_agent_execution_from_id(self.session, self.agent_execution_id),
+                session=self.session
+            )
+            if resource.storage_type == StorageType.S3.value:
+                s3_helper = S3Helper()
+                s3_helper.upload_file(img, path=resource.path)
+
+    def write_file(self, file_name: str, content: str) -> str:
+        if self.agent_id is not None:
+            final_path = ResourceHelper.get_agent_write_resource_path(
+                file_name,
+                agent=Agent.get_agent_from_id(self.session, self.agent_id),
+                agent_execution=AgentExecution.get_agent_execution_from_id(self.session, self.agent_execution_id)
+            )
+        else:
+            final_path = ResourceHelper.get_resource_path(file_name)
+        try:
+            with open(final_path, mode="w") as file:
+                file.write(content)
+            self.write_to_s3(file_name, final_path)
+            logger.info(f"{file_name} - File written successfully")
+            return f"{file_name} - File written successfully"
+        except Exception as err:
+            return f"Error write_file: {err}"
+
+    def write_csv_file(self, file_name: str, csv_data: Any) -> str:
+        if self.agent_id is not None:
+            final_path = ResourceHelper.get_agent_write_resource_path(
+                file_name,
+                agent=Agent.get_agent_from_id(self.session, self.agent_id),
+                agent_execution=AgentExecution.get_agent_execution_from_id(self.session, self.agent_execution_id)
+            )
+        else:
+            final_path = ResourceHelper.get_resource_path(file_name)
+        try:
+            with open(final_path, mode="w", newline="") as file:
+                writer = csv.writer(file, lineterminator="\n")
+                writer.writerows(csv_data)
+            self.write_to_s3(file_name, final_path)
+            logger.info(f"{file_name} - File written successfully")
+            return f"{file_name} - File written successfully"
+        except Exception as err:
+            return f"Error write_csv_file: {err}"
+
+    def get_agent_resource_path(self, file_name: str) -> str:
+        return ResourceHelper.get_agent_write_resource_path(
+            file_name,
+            agent=Agent.get_agent_from_id(self.session, self.agent_id),
+            agent_execution=AgentExecution.get_agent_execution_from_id(self.session, self.agent_execution_id)
+        )
+
+    def read_file(self, file_name: str) -> str:
+        if self.agent_id is not None:
+            final_path = self.get_agent_resource_path(file_name)
+        else:
+            final_path = ResourceHelper.get_resource_path(file_name)
+
+        try:
+            with open(final_path, mode="r") as file:
+                content = file.read()
+            logger.info(f"{file_name} - File read successfully")
+            return content
+        except Exception as err:
+            return f"Error while reading file {file_name}: {err}"
+
+    def get_files(self) -> list[str]:
+        """
+        Gets all file names generated by the CodingTool.
+        Returns:
+            A list of file names.
+        """
+
+        if self.agent_id is not None:
+            final_path = self.get_agent_resource_path("")
+        else:
+            final_path = ResourceHelper.get_resource_path("")
+        try:
+            # List all files in the directory
+            files = os.listdir(final_path)
+        except Exception as err:
+            logger.error(f"Error while accessing files in {final_path}: {err}")
+            files = []
+        return files
+```
+
+gui/package.json
+```json
+gui/package.json
+```
+```json
+{
+  "name": "super-agi",
+  "version": "0.1.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "lint": "next lint",
+    "export": "next export"
+  },
+  "dependencies": {
+    "axios": "^1.4.0",
+    "bootstrap": "^5.2.3",
+    "date-fns": "^2.30.0",
+    "date-fns-tz": "^2.0.0",
+    "echarts": "^5.4.2",
+    "echarts-for-react": "^3.0.2",
+    "eslint": "8.40.0",
+    "eslint-config-next": "13.4.2",
+    "js-cookie": "^3.0.5",
+    "jszip": "^3.10.1",
+    "mitt": "^3.0.0",
+    "mixpanel-browser": "^2.47.0",
+    "moment": "^2.29.4",
+    "moment-timezone": "^0.5.43",
+    "next": "13.4.2",
+    "react": "18.2.0",
+    "react-datetime": "^3.2.0",
+    "react-dom": "18.2.0",
+    "react-draggable": "^4.4.5",
+    "react-grid-layout": "^1.3.4",
+    "react-markdown": "^8.0.7",
+    "react-spinners": "^0.13.8",
+    "react-tippy": "^1.4.0",
+    "react-toastify": "^9.1.3"
+  }
+}
+```
