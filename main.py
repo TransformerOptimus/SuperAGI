5bf4ac140c1e5df1de61ee1e60eb11e8cb0ca298
Rounak Bhatia <rounak@contlo.com>
2023-10-31 16:36:44 +0530

    fixes

--- main.py
+++ main.py
@@ -216,13 +216,6 @@ async def startup_event():
             Organisation.id == marketplace_organisation_id).first()
         if marketplace_organisation is not None:
             register_marketplace_toolkits(session, marketplace_organisation)
-    
-    def local_llm_model_config():
-        existing_models_config = session.query(ModelsConfig).filter(ModelsConfig.org_id == default_user.organisation_id, ModelsConfig.provider == 'Local LLM').first()
-        if existing_models_config is None:
-            models_config = ModelsConfig(org_id=default_user.organisation_id, provider='Local LLM', api_key="EMPTY")
-            session.add(models_config)
-            session.commit()
 
     IterationWorkflowSeed.build_single_step_agent(session)
     IterationWorkflowSeed.build_task_based_agents(session)
@@ -246,8 +239,7 @@ async def startup_event():
     # AgentWorkflowSeed.doc_search_and_code(session)
     # AgentWorkflowSeed.build_research_email_workflow(session)
     replace_old_iteration_workflows(session)
-    local_llm_model_config()
-    
+
     if env != "PROD":
         register_toolkit_for_all_organisation()
     else:
