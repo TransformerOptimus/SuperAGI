99b2582246f0f1489d22cd12d75d1cc7f15a1045
edevaan <34842424+edevaan@users.noreply.github.com>
2023-11-15 16:32:16 -0600

    Add files via upload

---
+++ gpt_engineer/__init__.py
@@ -0,0 +1,8 @@
+# Adding convenience imports to the package
+from gpt_engineer.core import (
+    ai,
+    domain,
+    chat_to_files,
+    steps,
+)
+from gpt_engineer.data import file_repository, code_vector_repository
