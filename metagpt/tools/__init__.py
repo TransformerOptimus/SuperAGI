f6e752ddec2f36b7c7dd289a9492d56d51ccc464
edevaan <34842424+edevaan@users.noreply.github.com>
2023-11-15 17:01:18 -0600

    Add files via upload

---
+++ metagpt/tools/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2023/4/29 15:35
+@Author  : alexanderwu
+@File    : __init__.py
+"""
+
+
+from enum import Enum
+
+
+class SearchEngineType(Enum):
+    SERPAPI_GOOGLE = "serpapi"
+    SERPER_GOOGLE = "serper"
+    DIRECT_GOOGLE = "google"
+    DUCK_DUCK_GO = "ddg"
+    CUSTOM_ENGINE = "custom"
+
+
+class WebBrowserEngineType(Enum):
+    PLAYWRIGHT = "playwright"
+    SELENIUM = "selenium"
+    CUSTOM = "custom"
