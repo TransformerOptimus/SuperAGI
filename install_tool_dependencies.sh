f4517362b9cc1229e38adcd5479f1b227d4153ac
Arcane_Fire <81794144+GaryOcean428@users.noreply.github.com>
2024-02-19 19:16:36 +0800

    Update install_tool_dependencies.sh
    updated reference to requirements_apt.txt and changed to requirements.txt
    
    Signed-off-by: Arcane_Fire <81794144+GaryOcean428@users.noreply.github.com>

--- install_tool_dependencies.sh
+++ install_tool_dependencies.sh
@@ -9,9 +9,9 @@ pip install -r /app/requirements.txt
 
 for tool in /app/superagi/tools/* /app/superagi/tools/external_tools/* /app/superagi/tools/marketplace_tools/* ; do
 # Loop through the tools directories and install their apt_requirements.txt if they exist
-  if [ -d "$tool" ] && [ -f "$tool/requirements_apt.txt" ]; then
+  if [ -d "$tool" ] && [ -f "$tool/requirements.txt" ]; then
     echo "Installing apt requirements for tool: $(basename "$tool")"
-    xargs apt install -y < "$tool/requirements_apt.txt"
+    xargs apt install -y < "$tool/requirements.txt"
   fi
 # Loop through the tools directories and install their requirements.txt if they exist
   if [ -d "$tool" ] && [ -f "$tool/requirements.txt" ]; then
